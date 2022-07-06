530e7854037873d89de7553bd72b5a4221a7a656
Martín Montes <martin11lrx@gmail.com>
2022-07-06 20:54:23 +0200

    Added grafana dashboards to local kube-prometheus-stack

--- scripts/install_prometheus.sh
+++ scripts/install_prometheus.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+CONFIG="$( dirname "${BASH_SOURCE[0]}" )"/config
+
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm upgrade --install \
   -n kube-prometheus-stack --create-namespace \
+  -f $CONFIG/kube-prometheus-stack.yaml \
   kube-prometheus-stack prometheus-community/kube-prometheus-stack
