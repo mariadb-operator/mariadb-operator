5d6f8850f7d7321b098852f450c38a8efff5d21c
Martín Montes <martin11lrx@gmail.com>
2022-09-12 22:02:49 +0200

    Added labels to RestoreMariaDB

--- pkg/builder/backup_builder.go
+++ pkg/builder/backup_builder.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	databasev1alpha1 "github.com/mmontes11/mariadb-operator/api/v1alpha1"
+	labels "github.com/mmontes11/mariadb-operator/pkg/builder/labels"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -12,10 +13,16 @@ import (
 
 func (b *Builder) BuildRestoreMariaDb(mariaDb *databasev1alpha1.MariaDB, backupRef corev1.LocalObjectReference,
 	key types.NamespacedName) (*databasev1alpha1.RestoreMariaDB, error) {
+	restoreLabels :=
+		labels.NewLabelsBuilder().
+			WithApp(appMariaDb).
+			WithInstance(mariaDb.Name).
+			Build()
 	restore := &databasev1alpha1.RestoreMariaDB{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      key.Name,
 			Namespace: key.Namespace,
+			Labels:    restoreLabels,
 		},
 		Spec: databasev1alpha1.RestoreMariaDBSpec{
 			MariaDBRef: corev1.LocalObjectReference{
