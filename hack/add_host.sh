cfa6ef8e6c13c3346723dc5f673f3ba6e42e623c
Martin Montes <martin11lrx@gmail.com>
2023-02-04 18:49:04 +0100

    Fixed default prometheus scrape internal and timeout

--- hack/add_host.sh
+++ hack/add_host.sh
@@ -9,5 +9,5 @@ if [ -n "$(grep $HOSTNAME /etc/hosts)" ]; then
   echo "\"$HOSTNAME\" host already exists in /etc/hosts"
 else
   echo "Adding \"$HOSTNAME\" to /etc/hosts";
-  sudo -- sh -c -e "echo '$IP\t$HOSTNAME' >> /etc/hosts";
+  sudo -- sh -c -e "echo '\n# mariadb-operator\n$IP\t$HOSTNAME' >> /etc/hosts";
 fi
