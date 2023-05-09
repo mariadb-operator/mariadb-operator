ee994984e2a13af0527f83034171a28c4e06aa6a
Vivi <vivi@vivide.re>
2023-05-09 15:56:31 +0000

    feat: reconcile User using custom username if defined

--- controllers/user_controller_finalizer.go
+++ controllers/user_controller_finalizer.go
@@ -66,7 +66,11 @@ func (wf *wrappedUserFinalizer) ContainsFinalizer() bool {
 }
 
 func (wf *wrappedUserFinalizer) Reconcile(ctx context.Context, mdbClient *mariadbclient.Client) error {
-	if err := mdbClient.DropUser(ctx, wf.user.Name); err != nil {
+	username := wf.user.Name
+	if wf.user.Spec.Name != "" {
+		username = wf.user.Spec.Name
+	}
+	if err := mdbClient.DropUser(ctx, username); err != nil {
 		return fmt.Errorf("error dropping user in MariaDB: %v", err)
 	}
 	return nil
