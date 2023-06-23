85e062ca91914e04bd6eab96e460582f7727785c
Julian Bui <julian.bui@appian.com>
2023-06-23 07:44:50 -0400

    AN-123456: Adding routing for new services (not sure if right)

--- make/net.mk
+++ make/net.mk
@@ -8,6 +8,7 @@ host-mariadb:  ## Add mariadb hosts to /etc/hosts.
 	@./hack/add_host.sh 172.18.0.13 mariadb-3.mariadb.default.svc.cluster.local
 	@./hack/add_host.sh 172.18.0.20 mariadb.default.svc.cluster.local
 	@./hack/add_host.sh 172.18.0.30 primary-mariadb.default.svc.cluster.local
+	@./hack/add_host.sh 172.18.0.40 mariadb-replica.default.svc.cluster.local
 
 .PHONY: host-mariadb-test
 host-mariadb-test: ## Add mariadb test hosts to /etc/hosts.
@@ -20,6 +21,7 @@ host-mariadb-repl: ## Add mariadb repl hosts to /etc/hosts.
 	@./hack/add_host.sh 172.18.0.112 mariadb-repl-2.mariadb-repl.default.svc.cluster.local
 	@./hack/add_host.sh 172.18.0.120 mariadb-repl.default.svc.cluster.local
 	@./hack/add_host.sh 172.18.0.130 primary-mariadb-repl.default.svc.cluster.local
+	@./hack/add_host.sh 172.18.0.140 mariadb-replica.default.svc.cluster.local
 
 .PHONY: net
 net: install-metallb host-mariadb host-mariadb-repl ## Configure networking for local development.
