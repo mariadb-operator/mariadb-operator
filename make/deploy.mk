ea98dc8a3c46ad296a7a23337bbcc7878e664aac
Martin Montes <martin11lrx@gmail.com>
2023-10-13 22:01:45 +0200

    Bump image

--- make/deploy.mk
+++ make/deploy.mk
@@ -4,7 +4,7 @@ CLUSTER ?= mdb
 ##@ Docker
 
 PLATFORM ?= linux/amd64,linux/arm64
-IMG ?= ghcr.io/mariadb-operator/mariadb-operator:v0.0.21
+IMG ?= ghcr.io/mariadb-operator/mariadb-operator:v0.0.22
 BUILDX ?= docker buildx build --platform $(PLATFORM) -t $(IMG) 
 BUILDER ?= mariadb-operator
 
