50c56c6a42c84bf513390bb850b6315ee33d8aaa
Martin Montes <martin11lrx@gmail.com>
2023-12-18 19:59:37 +0100

    Added point in time recovery logic

--- pkg/pitr/pitr.go
+++ pkg/pitr/pitr.go
@@ -1,7 +1,66 @@
 package pitr
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"sort"
+	"strings"
+	"time"
 
-func GetTargetRecoveryFile(backupFileNames []string, targetRecoveryTime time.Time) (string, error) {
-	return "backup.2023-12-18T16:01:00Z.sql", nil
+	"github.com/go-logr/logr"
+)
+
+type backupDiff struct {
+	fileName string
+	diff     time.Duration
+}
+
+// GetTargetRecoveryFile finds the backup file with the closest date to the target recovery time.
+func GetTargetRecoveryFile(backupFileNames []string, targetRecoveryTime time.Time, logger logr.Logger) (string, error) {
+	var backupDiffs []backupDiff
+	for _, file := range backupFileNames {
+		backupDate, err := parseBackupDate(file)
+		if err != nil {
+			logger.Error(err, "error parsing backup date. Skipping", "file", file)
+			continue
+		}
+		diff := backupDate.Sub(targetRecoveryTime).Abs()
+		if diff == 0 {
+			return file, nil
+		}
+		backupDiffs = append(backupDiffs, backupDiff{
+			fileName: file,
+			diff:     diff,
+		})
+	}
+	if len(backupDiffs) == 0 {
+		return "", errors.New("no valid backup files were found")
+	}
+
+	sort.Slice(backupDiffs, func(i, j int) bool {
+		return backupDiffs[i].diff < backupDiffs[j].diff
+	})
+	return backupDiffs[0].fileName, nil
+}
+
+// IsValidBackupFile determines whether a backup file name is valid.
+func IsValidBackupFile(fileName string) bool {
+	if !strings.HasPrefix(fileName, "backup.") || !strings.HasSuffix(fileName, ".sql") {
+		return false
+	}
+	_, err := parseBackupDate(fileName)
+	return err == nil
+}
+
+func parseBackupDate(fileName string) (time.Time, error) {
+	parts := strings.Split(fileName, ".")
+	if len(parts) != 3 {
+		return time.Time{}, fmt.Errorf("invalid file name: %s", fileName)
+	}
+	timeRaw := parts[1]
+	t, err := time.Parse(time.RFC3339, timeRaw)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("error parsing file date: %v", err)
+	}
+	return t, nil
 }
---
+++ pkg/pitr/ptr_test.go
@@ -0,0 +1,183 @@
+package pitr
+
+import (
+	"testing"
+	"time"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func TestIsValidBackupFile(t *testing.T) {
+	tests := []struct {
+		name       string
+		backupFile string
+		wantValid  bool
+	}{
+		{
+			name:       "empty",
+			backupFile: "",
+			wantValid:  false,
+		},
+		{
+			name:       "no date",
+			backupFile: "backup.sql",
+			wantValid:  false,
+		},
+		{
+			name:       "no prefix",
+			backupFile: "2023-12-18 16:14.sql",
+			wantValid:  false,
+		},
+		{
+			name:       "no extension",
+			backupFile: "backup.2023-12-18T16:14:00Z",
+			wantValid:  false,
+		},
+		{
+			name:       "invalid date",
+			backupFile: "backup.2023-12-18 16:14.sql",
+			wantValid:  false,
+		},
+		{
+			name:       "valid",
+			backupFile: "backup.2023-12-18T16:14:00Z.sql",
+			wantValid:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			valid := IsValidBackupFile(tt.backupFile)
+			if tt.wantValid != valid {
+				t.Fatalf("unexpected backup file validity, expected: %v got: %v", tt.wantValid, valid)
+			}
+		})
+	}
+}
+
+func TestGetTargerRecoveryFile(t *testing.T) {
+	logger := ctrl.Log.WithName("test")
+	tests := []struct {
+		name           string
+		backupFiles    []string
+		targetRecovery time.Time
+		wantFile       string
+		wantErr        bool
+	}{
+		{
+			name:           "no backups",
+			backupFiles:    []string{},
+			targetRecovery: time.Now(),
+			wantFile:       "",
+			wantErr:        true,
+		},
+		{
+			name: "single backup",
+			backupFiles: []string{
+				"backup.2023-12-18T15:58:00Z.sql",
+			},
+			targetRecovery: time.UnixMilli(0),
+			wantFile:       "backup.2023-12-18T15:58:00Z.sql",
+			wantErr:        false,
+		},
+		{
+			name: "target before backups",
+			backupFiles: []string{
+				"backup.2023-12-18T15:58:00Z.sql",
+				"backup.2023-12-18T15:59:00Z.sql",
+				"backup.2023-12-18T16:00:00Z.sql",
+				"backup.2023-12-18T16:03:00Z.sql",
+				"backup.2023-12-18T16:07:00Z.sql",
+				"backup.2023-12-18T16:08:00Z.sql",
+				"backup.2023-12-18T16:09:00Z.sql",
+				"backup.2023-12-18T16:12:00Z.sql",
+				"backup.2023-12-18T16:13:00Z.sql",
+			},
+			targetRecovery: time.UnixMilli(0),
+			wantFile:       "backup.2023-12-18T15:58:00Z.sql",
+			wantErr:        false,
+		},
+		{
+			name: "target after backups",
+			backupFiles: []string{
+				"backup.2023-12-18T15:58:00Z.sql",
+				"backup.2023-12-18T15:59:00Z.sql",
+				"backup.2023-12-18T16:00:00Z.sql",
+				"backup.2023-12-18T16:03:00Z.sql",
+				"backup.2023-12-18T16:07:00Z.sql",
+				"backup.2023-12-18T16:08:00Z.sql",
+				"backup.2023-12-18T16:09:00Z.sql",
+				"backup.2023-12-18T16:12:00Z.sql",
+				"backup.2023-12-18T16:13:00Z.sql",
+			},
+			targetRecovery: time.Now(),
+			wantFile:       "backup.2023-12-18T16:13:00Z.sql",
+			wantErr:        false,
+		},
+		{
+			name: "close target",
+			backupFiles: []string{
+				"backup.2023-12-18T15:58:00Z.sql",
+				"backup.2023-12-18T15:59:00Z.sql",
+				"backup.2023-12-18T16:00:00Z.sql",
+				"backup.2023-12-18T16:03:00Z.sql",
+				"backup.2023-12-18T16:07:00Z.sql",
+				"backup.2023-12-18T16:08:00Z.sql",
+				"backup.2023-12-18T16:09:00Z.sql",
+				"backup.2023-12-18T16:12:00Z.sql",
+				"backup.2023-12-18T16:13:00Z.sql",
+			},
+			targetRecovery: func() time.Time {
+				target, err := time.Parse(time.RFC3339, "2023-12-18T16:04:00Z")
+				if err != nil {
+					t.Fatalf("unexpected error parsing date: %v", err)
+				}
+				return target
+			}(),
+			wantFile: "backup.2023-12-18T16:03:00Z.sql",
+			wantErr:  false,
+		},
+		{
+			name: "exact target",
+			backupFiles: []string{
+				"backup.2023-12-18T15:58:00Z.sql",
+				"backup.2023-12-18T15:59:00Z.sql",
+				"backup.2023-12-18T16:00:00Z.sql",
+				"backup.2023-12-18T16:03:00Z.sql",
+				"backup.2023-12-18T16:07:00Z.sql",
+				"backup.2023-12-18T16:08:00Z.sql",
+				"backup.2023-12-18T16:09:00Z.sql",
+				"backup.2023-12-18T16:12:00Z.sql",
+				"backup.2023-12-18T16:13:00Z.sql",
+			},
+			targetRecovery: func() time.Time {
+				target, err := time.Parse(time.RFC3339, "2023-12-18T16:07:00Z")
+				if err != nil {
+					t.Fatalf("unexpected error parsing date: %v", err)
+				}
+				return target
+			}(),
+			wantFile: "backup.2023-12-18T16:07:00Z.sql",
+			wantErr:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			file, err := GetTargetRecoveryFile(tt.backupFiles, tt.targetRecovery, logger)
+			if err != nil && !tt.wantErr {
+				t.Fatalf("unexpected error getting target recovery file: %v", err)
+			}
+
+			if tt.wantFile != file {
+				t.Fatalf("unexpected backup target file, expected: %v got: %v", tt.wantFile, file)
+			}
+			if tt.wantErr && err == nil {
+				t.Error("expect error to have occurred, got nil")
+			}
+			if !tt.wantErr && err != nil {
+				t.Errorf("expect error to not have occurred, got: %v", err)
+			}
+		})
+	}
+}
