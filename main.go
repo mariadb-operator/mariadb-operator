0f82fd04ee2fb8dbe996fcfbca806bc7bfada3d8
Martin Montes <martin11lrx@gmail.com>
2024-01-07 15:33:32 +0100

    Added main.go to be able to generate APIs and controllers with kubebuilder

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// See:
+	// - cmd/controller/main.go
+	// - https://github.com/kubernetes-sigs/kubebuilder/issues/932
+	// This main.go is required by kubebuilder to scaffold APIs and controllers.
+}
