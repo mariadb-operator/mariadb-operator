e7c155b09f27fdc4e56643aea453eaa85b89d538
Martin Montes <martin11lrx@gmail.com>
2024-01-13 19:16:18 +0100

    Configurable MaxScale API request logs

--- pkg/http/request.go
+++ pkg/http/request.go
@@ -1,4 +1,4 @@
-package client
+package http
 
 import (
 	"bytes"
@@ -20,7 +20,6 @@ func (c *Client) NewRequestWithContext(ctx context.Context, method string, path 
 	}
 
 	if method == http.MethodGet {
-		c.logRequest(method, baseUrl, nil)
 		req, err := http.NewRequestWithContext(ctx, method, baseUrl.String(), nil)
 		if err != nil {
 			return nil, fmt.Errorf("error creating GET request: %v", err)
@@ -31,7 +30,6 @@ func (c *Client) NewRequestWithContext(ctx context.Context, method string, path 
 	var bodyReader io.Reader
 	if body != nil {
 		bodyBytes, err := json.Marshal(body)
-		c.logRequest(method, baseUrl, bodyBytes)
 		if err != nil {
 			return nil, fmt.Errorf("error encoding body: %v", err)
 		}
@@ -64,24 +62,4 @@ func (c *Client) buildUrl(path string, query map[string]string) (*url.URL, error
 		return nil, err
 	}
 	return newUrl, nil
-}
-
-func (c *Client) logRequest(method string, url *url.URL, body []byte) {
-	kv := []interface{}{
-		"method",
-		method,
-		"URL",
-		url.String(),
-	}
-	if body != nil {
-		kv = append(kv, "body", string(body))
-	}
-	c.logDebug("Request", kv...)
-}
-
-func (c *Client) logDebug(msg string, kv ...interface{}) {
-	if c.logger == nil {
-		return
-	}
-	c.logger.V(1).Info(msg, kv...)
 }
