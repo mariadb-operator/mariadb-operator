23d1bb561056916f3526c8de5b42c380f5158e71
Martin Montes <martin11lrx@gmail.com>
2024-02-15 15:21:49 +0100

    Support for getting MariaDBs in kubernetes clientSet

--- pkg/kubernetes/clientset/clientset.go
+++ pkg/kubernetes/clientset/clientset.go
@@ -1,24 +1,46 @@
 package kubeclientset
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
+	mariadbv1alpha1 "github.com/mariadb-operator/mariadb-operator/api/v1alpha1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func NewClientSet() (*kubernetes.Clientset, error) {
+type ClientSet struct {
+	kubernetes.Clientset
+}
+
+func NewClientSet() (*ClientSet, error) {
 	restConfig, err := restConfig()
 	if err != nil {
 		return nil, fmt.Errorf("error getting REST config: %v", err)
 	}
-	clientset, err := kubernetes.NewForConfig(restConfig)
+	clientSet, err := kubernetes.NewForConfig(restConfig)
 	if err != nil {
 		return nil, fmt.Errorf("error creating clientset: %v", err)
 	}
-	return clientset, nil
+	return &ClientSet{
+		Clientset: *clientSet,
+	}, nil
+}
+
+func (c *ClientSet) GetMariaDB(ctx context.Context, name, namespace string) (*mariadbv1alpha1.MariaDB, error) {
+	path := fmt.Sprintf("/apis/mariadb.mmontes.io/v1alpha1/namespaces/%s/mariadbs/%s", namespace, name)
+	bytes, err := c.RESTClient().Get().AbsPath(path).DoRaw(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error requesting '%s' MariaDB in namespace '%s': %v", name, namespace, err)
+	}
+	var mdb mariadbv1alpha1.MariaDB
+	if err := json.Unmarshal(bytes, &mdb); err != nil {
+		return nil, fmt.Errorf("error decoding MariaDB: %v", err)
+	}
+	return &mdb, nil
 }
 
 func restConfig() (*rest.Config, error) {
