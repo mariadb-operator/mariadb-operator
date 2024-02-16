8a99073326b2067b83201abc7863f083bf7d390c
Martin Montes <martin11lrx@gmail.com>
2024-02-16 16:25:12 +0100

    Improved probes logging

--- pkg/galera/agent/handler/probe.go
+++ pkg/galera/agent/handler/probe.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -19,40 +18,44 @@ import (
 )
 
 type Probe struct {
-	mariadbKey     types.NamespacedName
-	k8sClient      ctrlclient.Client
-	refResolver    *refresolver.RefResolver
-	responseWriter *mdbhttp.ResponseWriter
-	logger         *logr.Logger
+	mariadbKey      types.NamespacedName
+	k8sClient       ctrlclient.Client
+	refResolver     *refresolver.RefResolver
+	responseWriter  *mdbhttp.ResponseWriter
+	livenessLogger  logr.Logger
+	readinessLogger logr.Logger
 }
 
 func NewProbe(mariadbKey types.NamespacedName, k8sClient ctrlclient.Client, responseWriter *mdbhttp.ResponseWriter,
 	logger *logr.Logger) *Probe {
 	return &Probe{
-		mariadbKey:     mariadbKey,
-		k8sClient:      k8sClient,
-		refResolver:    refresolver.New(k8sClient),
-		responseWriter: responseWriter,
-		logger:         logger,
+		mariadbKey:      mariadbKey,
+		k8sClient:       k8sClient,
+		refResolver:     refresolver.New(k8sClient),
+		responseWriter:  responseWriter,
+		livenessLogger:  logger.WithName("liveness"),
+		readinessLogger: logger.WithName("readiness"),
 	}
 }
 
 func (p *Probe) Liveness(w http.ResponseWriter, r *http.Request) {
+	p.livenessLogger.V(1).Info("Probe started")
 	var mdb mariadbv1alpha1.MariaDB
 	if err := p.k8sClient.Get(r.Context(), p.mariadbKey, &mdb); err != nil {
-		p.logger.Error(err, "error getting MariaDB")
+		p.livenessLogger.Error(err, "error getting MariaDB")
 		p.responseWriter.WriteError(w, "error getting MariaDB")
 		return
 	}
 	// avoid restarting Pods during cluster recovery
 	if mdb.HasGaleraNotReadyCondition() {
+		p.livenessLogger.Info("Galera not ready. Returning OK")
 		p.responseWriter.WriteOK(w, nil)
 		return
 	}
 
 	sqlClient, err := p.getSqlClient(r.Context(), &mdb)
 	if err != nil {
-		p.logger.Error(err, "error getting SQL client")
+		p.livenessLogger.Error(err, "error getting SQL client")
 		p.responseWriter.WriteError(w, "error getting SQL client")
 		return
 	}
@@ -60,12 +63,12 @@ func (p *Probe) Liveness(w http.ResponseWriter, r *http.Request) {
 
 	status, err := sqlClient.GaleraClusterStatus(r.Context())
 	if err != nil {
-		p.logger.Error(err, "error getting cluster status")
+		p.livenessLogger.Error(err, "error getting cluster status")
 		p.responseWriter.WriteError(w, "error getting cluster status")
 		return
 	}
 	if status != "Primary" {
-		p.logger.Error(errors.New("MariaDB Galera is unhealthy"), "status", status)
+		p.livenessLogger.Info("MariaDB Galera is unhealthy", "status", status)
 		p.responseWriter.WriteErrorf(w, "MariaDB Galera is unhealthy. Status: %s", status)
 		return
 	}
@@ -74,21 +77,23 @@ func (p *Probe) Liveness(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Probe) Readiness(w http.ResponseWriter, r *http.Request) {
+	p.readinessLogger.V(1).Info("Probe started")
 	var mdb mariadbv1alpha1.MariaDB
 	if err := p.k8sClient.Get(r.Context(), p.mariadbKey, &mdb); err != nil {
-		p.logger.Error(err, "error getting MariaDB")
+		p.readinessLogger.Error(err, "error getting MariaDB")
 		p.responseWriter.WriteError(w, "error getting MariaDB")
 		return
 	}
 	// keep sending traffic to Pods during cluster recovery
 	if mdb.HasGaleraNotReadyCondition() {
+		p.readinessLogger.Info("Galera not ready. Returning OK")
 		p.responseWriter.WriteOK(w, nil)
 		return
 	}
 
 	sqlClient, err := p.getSqlClient(r.Context(), &mdb)
 	if err != nil {
-		p.logger.Error(err, "error getting SQL client")
+		p.readinessLogger.Error(err, "error getting SQL client")
 		p.responseWriter.WriteError(w, "error getting SQL client")
 		return
 	}
@@ -96,24 +101,24 @@ func (p *Probe) Readiness(w http.ResponseWriter, r *http.Request) {
 
 	status, err := sqlClient.GaleraClusterStatus(r.Context())
 	if err != nil {
-		p.logger.Error(err, "error getting cluster status")
+		p.readinessLogger.Error(err, "error getting cluster status")
 		p.responseWriter.WriteError(w, "error getting cluster status")
 		return
 	}
 	if status != "Primary" {
-		p.logger.Error(errors.New("MariaDB Galera is unhealthy"), "status", status)
+		p.readinessLogger.Info("MariaDB Galera is unhealthy", "status", status)
 		p.responseWriter.WriteErrorf(w, "MariaDB Galera is unhealthy. Status: %s", status)
 		return
 	}
 
 	state, err := sqlClient.GaleraLocalState(r.Context())
 	if err != nil {
-		p.logger.Error(err, "error getting local state")
+		p.readinessLogger.Error(err, "error getting local state")
 		p.responseWriter.WriteError(w, "error getting local state")
 		return
 	}
-	if state != "Synced" && state != "Donor" {
-		p.logger.Error(errors.New("MariaDB Galera is not synced"), "state", state)
+	if state != "Synced" && state != "Donor/Desynced" {
+		p.readinessLogger.Info("MariaDB Galera is not synced", "state", state)
 		p.responseWriter.WriteErrorf(w, "MariaDB Galera is not synced. State: %s", state)
 		return
 	}
