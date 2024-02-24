023bdf23098cf140a6ecb4ba5c0457ea8fbb0608
Martin Montes <martin11lrx@gmail.com>
2024-02-24 16:30:14 +0100

    Consider file system resizing condition to determine whether a PVC is resizing

--- pkg/pvc/pvc.go
+++ pkg/pvc/pvc.go
@@ -2,7 +2,26 @@ package pvc
 
 import corev1 "k8s.io/api/core/v1"
 
-func IsResizing(pvc corev1.PersistentVolumeClaim) bool {
+// IsResizing returns true if the PVC is resizing
+func IsResizing(pvc *corev1.PersistentVolumeClaim) bool {
+	return IsPersistentVolumeClaimFileSystemResizePending(pvc) || IsPersistentVolumeClaimResizing(pvc)
+}
+
+// IsPersistentVolumeClaimFileSystemResizePending returns true if the PVC has FileSystemResizePending condition set to true
+func IsPersistentVolumeClaimFileSystemResizePending(pvc *corev1.PersistentVolumeClaim) bool {
+	for _, c := range pvc.Status.Conditions {
+		if c.Status != corev1.ConditionTrue {
+			continue
+		}
+		if c.Type == corev1.PersistentVolumeClaimFileSystemResizePending {
+			return true
+		}
+	}
+	return false
+}
+
+// IsPersistentVolumeClaimResizing returns true if the PVC has Resizing condition set to true
+func IsPersistentVolumeClaimResizing(pvc *corev1.PersistentVolumeClaim) bool {
 	for _, condition := range pvc.Status.Conditions {
 		if condition.Status != corev1.ConditionTrue {
 			continue
