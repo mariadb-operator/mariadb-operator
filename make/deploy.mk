4f7daee50dcecc899ac61d6e147607096c218874
Martin Montes <martin11lrx@gmail.com>
2024-03-08 14:58:03 +0100

    Bump kind and kubernetes

--- make/deploy.mk
+++ make/deploy.mk
@@ -4,7 +4,7 @@ CLUSTER ?= mdb
 ##@ Cluster
 
 KIND_CONFIG ?= hack/config/kind.yaml
-KIND_IMAGE ?= kindest/node:v1.28.0
+KIND_IMAGE ?= kindest/node:v1.29.2
 
 .PHONY: cluster
 cluster: kind ## Create a single node kind cluster.
