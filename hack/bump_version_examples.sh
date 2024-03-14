1f72b173a4795b7169cc031c26cfd0c389a4e337
Martin Montes <martin11lrx@gmail.com>
2024-03-14 18:00:37 +0100

    Update operator image in examples

---
+++ hack/bump_version_examples.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FOLDER="$1"
+IMAGE="$2"
+VERSION="$3"
+
+if [ -z "$FOLDER" ] || [ -z "$IMAGE" ] || [ -z "$VERSION" ]; then
+  echo "Usage: $0 path/examples ghcr.io/mariadb-operator/mariadb-operator v0.0.28"
+  exit 1
+fi
+
+echo "Updating examples in folder '$FOLDER' to '$IMAGE:$VERSION'"
+
+for file in "$FOLDER"/*.yaml; do
+  if [ -f "$file" ] && [ -r "$file" ]; then
+    sed -i "s|$IMAGE:[^ ]*|$IMAGE:$VERSION|g" "$file"
+  else
+    echo "Error: $file does not exist or is not readable."
+  fi
+done
