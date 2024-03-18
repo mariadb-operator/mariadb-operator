173ba6c16d0b56f3bd2b2a5224f955f40e54f775
Martin Montes <martin11lrx@gmail.com>
2024-03-18 16:43:19 +0100

    Fix envtest

--- make/deps.mk
+++ make/deps.mk
@@ -76,7 +76,7 @@ $(CONTROLLER_GEN): $(LOCALBIN)
 .PHONY: envtest
 envtest: $(ENVTEST) ## Download envtest-setup locally if necessary.
 $(ENVTEST): $(LOCALBIN)
-	GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-runtime/tools/setup-envtest@$(ENVTEST_K8S_VERSION)
+	GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
 
 .PHONY: ginkgo
 ginkgo: $(GINKGO) ## Download ginkgo locally if necessary.
