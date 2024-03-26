1bd9ebe43d3295b23ab337a37846c93326e3d8b5
Dawid Jakobczak <d.jakobczak@samsung.com>
2024-03-26 13:51:52 +0100

    maxscale: set default config based on memory limits
    - set threads to auto if cpu limit is not defined
    - set threads to cpu limit rounded up if cpu limit is defined
    - do not set query_classifier_cache_size if memory limit is not defined
    - set query_classifier_cache_size to 15% of memory limit if memory limit is defined
    - till allow to override those options with mxs.spec.config.params
    
    Signed-off-by: Dawid Jakobczak <d.jakobczak@samsung.com>

--- pkg/maxscale/config/config.go
+++ pkg/maxscale/config/config.go
@@ -10,29 +10,34 @@ import (
 )
 
 type tplOpts struct {
-	Threads               string
-	PersistRuntimeChanges bool
-	LoadPersistentConfigs bool
-	AdminHost             string
-	AdminPort             int32
-	AdminGui              bool
-	AdminSecureGui        bool
-	Params                map[string]string
+	Threads                  string
+	QueryClassifierCacheSize string
+	PersistRuntimeChanges    bool
+	LoadPersistentConfigs    bool
+	AdminHost                string
+	AdminPort                int32
+	AdminGui                 bool
+	AdminSecureGui           bool
+	Params                   map[string]string
 }
 
 var existingConfigKeys = map[string]struct{}{
-	"threads":                 {},
-	"persist_runtime_changes": {},
-	"load_persisted_configs":  {},
-	"admin_host":              {},
-	"admin_port":              {},
-	"admin_gui":               {},
-	"admin_secure_gui":        {},
+	"threads":                     {},
+	"query_classifier_cache_size": {},
+	"persist_runtime_changes":     {},
+	"load_persisted_configs":      {},
+	"admin_host":                  {},
+	"admin_port":                  {},
+	"admin_gui":                   {},
+	"admin_secure_gui":            {},
 }
 
 func Config(mxs *mariadbv1alpha1.MaxScale) ([]byte, error) {
 	tpl := createTpl(mxs.ConfigSecretKeyRef().Key, `[maxscale]
 threads={{ .Threads }}
+{{- if .QueryClassifierCacheSize }}
+query_classifier_cache_size={{ .QueryClassifierCacheSize }}
+{{- end }}
 persist_runtime_changes={{ .PersistRuntimeChanges }}
 load_persisted_configs={{ .LoadPersistentConfigs }}
 admin_host={{ .AdminHost }}
@@ -44,14 +49,15 @@ admin_secure_gui={{ .AdminSecureGui }}
 {{ end }}`)
 	buf := new(bytes.Buffer)
 	err := tpl.Execute(buf, tplOpts{
-		Threads:               configValueOrDefault("threads", mxs.Spec.Config.Params, "auto"),
-		PersistRuntimeChanges: true,
-		LoadPersistentConfigs: true,
-		AdminHost:             configValueOrDefault("admin_host", mxs.Spec.Config.Params, "0.0.0.0"),
-		AdminPort:             mxs.Spec.Admin.Port,
-		AdminGui:              ptr.Deref(mxs.Spec.Admin.GuiEnabled, true),
-		AdminSecureGui:        false,
-		Params:                filterExistingConfig(mxs.Spec.Config.Params),
+		Threads:                  configValueOrDefault("threads", mxs.Spec.Config.Params, threads(mxs)),
+		QueryClassifierCacheSize: configValueOrDefault("query_classifier_cache_size", mxs.Spec.Config.Params, queryClassifierCacheSize(mxs)),
+		PersistRuntimeChanges:    true,
+		LoadPersistentConfigs:    true,
+		AdminHost:                configValueOrDefault("admin_host", mxs.Spec.Config.Params, "0.0.0.0"),
+		AdminPort:                mxs.Spec.Admin.Port,
+		AdminGui:                 ptr.Deref(mxs.Spec.Admin.GuiEnabled, true),
+		AdminSecureGui:           false,
+		Params:                   filterExistingConfig(mxs.Spec.Config.Params),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("error rendering MaxScale config: %v", err)
