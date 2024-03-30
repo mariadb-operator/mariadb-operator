5892e71dc55605bbcf05adcaf66b3ffcfa600190
Martin Montes <martin11lrx@gmail.com>
2024-03-30 14:29:27 +0100

    Refactor meta support for PDB

---
+++ pkg/builder/poddisruptionbudget_builder_test.go
@@ -0,0 +1,56 @@
+package builder
+
+import (
+	"testing"
+
+	mariadbv1alpha1 "github.com/mariadb-operator/mariadb-operator/api/v1alpha1"
+)
+
+func TestPodDisruptionBudgetMeta(t *testing.T) {
+	builder := newTestBuilder()
+	tests := []struct {
+		name     string
+		opts     PodDisruptionBudgetOpts
+		wantMeta *mariadbv1alpha1.Metadata
+	}{
+		{
+			name: "no meta",
+			opts: PodDisruptionBudgetOpts{},
+			wantMeta: &mariadbv1alpha1.Metadata{
+				Labels:      map[string]string{},
+				Annotations: map[string]string{},
+			},
+		},
+		{
+			name: "meta",
+			opts: PodDisruptionBudgetOpts{
+				Metadata: &mariadbv1alpha1.Metadata{
+					Labels: map[string]string{
+						"database.myorg.io": "mariadb",
+					},
+					Annotations: map[string]string{
+						"database.myorg.io": "mariadb",
+					},
+				},
+			},
+			wantMeta: &mariadbv1alpha1.Metadata{
+				Labels: map[string]string{
+					"database.myorg.io": "mariadb",
+				},
+				Annotations: map[string]string{
+					"database.myorg.io": "mariadb",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			configMap, err := builder.BuildPodDisruptionBudget(tt.opts, &mariadbv1alpha1.MariaDB{})
+			if err != nil {
+				t.Fatalf("unexpected error building PDB: %v", err)
+			}
+			assertMeta(t, &configMap.ObjectMeta, tt.wantMeta.Labels, tt.wantMeta.Annotations)
+		})
+	}
+}
