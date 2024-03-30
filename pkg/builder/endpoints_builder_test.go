dfde9c099c0212bb80d7978e19525a652ee9ddef
Martin Montes <martin11lrx@gmail.com>
2024-03-30 13:10:14 +0100

    Refactor meta support for Endpoints

---
+++ pkg/builder/endpoints_builder_test.go
@@ -0,0 +1,64 @@
+package builder
+
+import (
+	"testing"
+
+	mariadbv1alpha1 "github.com/mariadb-operator/mariadb-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestEndpointsmeta(t *testing.T) {
+	builder := newTestBuilder()
+	key := types.NamespacedName{
+		Name: "endpoints",
+	}
+	subsets := []corev1.EndpointSubset{}
+	tests := []struct {
+		name     string
+		mariadb  *mariadbv1alpha1.MariaDB
+		wantMeta *mariadbv1alpha1.Metadata
+	}{
+		{
+			name:    "no meta",
+			mariadb: &mariadbv1alpha1.MariaDB{},
+			wantMeta: &mariadbv1alpha1.Metadata{
+				Labels:      map[string]string{},
+				Annotations: map[string]string{},
+			},
+		},
+		{
+			name: "meta",
+			mariadb: &mariadbv1alpha1.MariaDB{
+				Spec: mariadbv1alpha1.MariaDBSpec{
+					InheritMetadata: &mariadbv1alpha1.Metadata{
+						Labels: map[string]string{
+							"database.myorg.io": "mariadb",
+						},
+						Annotations: map[string]string{
+							"database.myorg.io": "mariadb",
+						},
+					},
+				},
+			},
+			wantMeta: &mariadbv1alpha1.Metadata{
+				Labels: map[string]string{
+					"database.myorg.io": "mariadb",
+				},
+				Annotations: map[string]string{
+					"database.myorg.io": "mariadb",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			endpoints, err := builder.BuildEndpoints(key, tt.mariadb, subsets)
+			if err != nil {
+				t.Fatalf("unexpected error building exporter Deployment: %v", err)
+			}
+			assertMeta(t, &endpoints.ObjectMeta, tt.wantMeta.Labels, tt.wantMeta.Annotations)
+		})
+	}
+}
