cb872ff88b6ffe8f00273cd91cced3c2c9f04fa9
Martin Montes <martin11lrx@gmail.com>
2024-04-09 11:06:20 +0200

    Updated certification test script

--- hack/certification_test.sh
+++ hack/certification_test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -euo pipefail
+set -Eeox pipefail
 
-# see: https://github.com/redhat-openshift-ecosystem/certification-releases/blob/main/4.9/ga/ci-pipeline.md
+# See: 
+# https://github.com/redhat-openshift-ecosystem/certification-releases/blob/main/4.9/ga/ci-pipeline.md
+# https://github.com/redhat-marketplace/redhat-marketplace-operator/blob/9af66069a49f2eea5b03d170dd3f85e56c19ebed/hack/certify/certify.sh
 
 if [ -z "$CERTIFIED_REPO" ]; then
   echo "CERTIFIED_REPO env variable must be provided"
@@ -16,61 +18,124 @@ if [ -z "$BUNDLE_PATH" ]; then
   echo "BUNDLE_PATH env variable must be provided"
   exit 1
 fi
-OPENSHIFT_VERSION="$(oc version -o json | jq -r .openshiftVersion)"
+SLEEP_LONG="${SLEEP_LONG:-5}"
+SLEEP_SHORT="${SLEEP_SHORT:-2}"
+CERT_NAMESPACE="${CERT_NAMESPACE:-certification}"
+KUBECONFIG="${KUBECONFIG:-$HOME/.kube/config}"
 
-if ! kubectl get ns certification > /dev/null 2>&1
-then
-  oc adm new-project certification
-fi
-oc project certification
+# Check Subscriptions: subscription-name, namespace
+checksub () {
+	echo "Waiting for Subscription $1 InstallPlan to complete."
 
-oc delete secret kubeconfig || true
-oc create secret generic kubeconfig --from-file=kubeconfig="$HOME/.kube/config"
+	# Wait 2 resync periods for OLM to emit new installplan
+	# sleep 60
 
-LATEST_OPENSHIFT_PIPELINES_OPERATOR_CSV="$(
-oc get packagemanifests openshift-pipelines-operator-rh \
-    --template '{{ range .status.channels }}{{ if eq .name "latest" }}{{ .currentCSV }}{{ "\n" }}{{ end }}{{ end }}')"
+	# Wait for the InstallPlan to be generated and available on status
+	unset INSTALL_PLAN
+	until oc get subscription $1 -n $2 --output=jsonpath={.status.installPlanRef.name}
+	do
+		sleep $SLEEP_SHORT
+	done
 
-cat << EOF | kubectl apply -f -
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: openshift-pipelines-operator-rh
-  namespace: openshift-operators
-spec:
-  channel: latest
-  installPlanApproval: Automatic
-  name: openshift-pipelines-operator-rh
-  source: redhat-operators
-  sourceNamespace: openshift-marketplace
-  startingCSV: $LATEST_OPENSHIFT_PIPELINES_OPERATOR_CSV
-EOF
+	# Get the InstallPlan
+	until [ -n "$INSTALL_PLAN" ]
+	do
+		sleep $SLEEP_SHORT
+		INSTALL_PLAN=$(oc get subscription $1 -n $2 --output=jsonpath={.status.installPlanRef.name})
+	done
 
-oc import-image certified-operator-index:v"${OPENSHIFT_VERSION%.*}" \
-  --from=registry.redhat.io/redhat/certified-operator-index:v"${OPENSHIFT_VERSION%.*}" \
+	# Wait for the InstallPlan to Complete
+	unset PHASE
+	until [ "$PHASE" == "Complete" ]
+	do
+		PHASE=$(oc get installplan $INSTALL_PLAN -n $2 --output=jsonpath={.status.phase})
+    if [ "$PHASE" == "Failed" ]; then
+      set +x
+      sleep 3
+      echo "InstallPlan $INSTALL_PLAN for subscription $1 failed."
+      echo "To investigate the reason of the InstallPlan failure run:"
+      echo "oc describe installplan $INSTALL_PLAN -n $2"
+      exit 1
+    fi
+		sleep $SLEEP_SHORT
+	done
+	
+	# Get installed CluserServiceVersion
+	unset CSV
+	until [ -n "$CSV" ]
+	do
+		sleep $SLEEP_SHORT
+		CSV=$(oc get subscription $1 -n $2 --output=jsonpath={.status.installedCSV})
+	done
+	
+	# Wait for the CSV
+	unset PHASE
+	until [ "$PHASE" == "Succeeded" ]
+	do
+		PHASE=$(oc get clusterserviceversion $CSV -n $2 --output=jsonpath={.status.phase})
+    if [ "$PHASE" == "Failed" ]; then
+      set +x
+      sleep 3
+      echo "ClusterServiceVersion $CSV for subscription $1 failed."
+      echo "To investigate the reason of the ClusterServiceVersion failure run:"
+      echo "oc describe clusterserviceversion $CSV -n $2"
+      exit 1
+    fi
+		sleep $SLEEP_SHORT
+	done
+}
+
+# Verify Subscriptions
+checksub openshift-pipelines-operator-rh openshift-operators
+
+# Switch to certification namespace
+oc delete ns $CERT_NAMESPACE --ignore-not-found
+oc adm new-project $CERT_NAMESPACE
+oc project $CERT_NAMESPACE
+
+# Switch to certification namespace
+oc delete ns $CERT_NAMESPACE --ignore-not-found
+oc adm new-project $CERT_NAMESPACE
+oc project $CERT_NAMESPACE
+
+# Wait for the tekton serviceaccount to generate
+echo "Waiting for ServiceAccount pipeline in namespace $CERT_NAMESPACE to generate."
+until oc -n $CERT_NAMESPACE get serviceaccount pipeline
+do
+  sleep 5s
+done
+
+# Create the kubeconfig used by the certification pipeline
+oc delete secret kubeconfig --ignore-not-found
+oc create secret generic kubeconfig --from-file=kubeconfig=$KUBECONFIG
+
+# Import redhat catalogs
+oc import-image certified-operator-index \
   --request-timeout=5m \
+  --from=registry.redhat.io/redhat/certified-operator-index \
   --reference-policy local \
   --scheduled \
-  --confirm > /dev/null
-oc tag registry.redhat.io/redhat/certified-operator-index:v"${OPENSHIFT_VERSION%.*}" certified-operator-index:v"${OPENSHIFT_VERSION%.*}"
+  --confirm \
+  --all
 
-oc import-image redhat-marketplace-index:v"${OPENSHIFT_VERSION%.*}" \
-  --from=registry.redhat.io/redhat/redhat-marketplace-index:v"${OPENSHIFT_VERSION%.*}" \
-  --request-timeout=5m \
-  --reference-policy local \
-  --scheduled \
-  --confirm > /dev/null
-oc tag registry.redhat.io/redhat/redhat-marketplace-index:v"${OPENSHIFT_VERSION%.*}" redhat-marketplace-index:v"${OPENSHIFT_VERSION%.*}"
-
-oc apply -f operator-pipelines/ansible/roles/operator-pipeline/templates/openshift/openshift-pipelines-custom-scc.yml
-oc adm policy add-scc-to-user pipelines-custom-scc -z pipeline
-
+# Install the Certification Pipeline
 if [ ! -d "operator-pipelines" ]; then
   git clone https://github.com/redhat-openshift-ecosystem/operator-pipelines
 fi
-oc apply -R -f operator-pipelines/ansible/roles/operator-pipeline/templates/openshift/pipelines/operator-ci-pipeline.yml
-oc apply -R -f operator-pipelines/ansible/roles/operator-pipeline/templates/openshift/tasks
+cd operator-pipelines
+git checkout v1.0.120
+cd -
 
+# Create a new SCC
+oc apply -f operator-pipelines/ansible/roles/operator-pipeline/templates/openshift/openshift-pipelines-custom-scc.yml
+# Add SCC to a pipeline service account
+oc adm policy add-scc-to-user pipelines-custom-scc -z pipeline
+
+# Workaround some files that fail webhook validation
+oc apply -R -f operator-pipelines/ansible/roles/operator-pipeline/templates/openshift/pipelines || true
+oc apply -R -f operator-pipelines/ansible/roles/operator-pipeline/templates/openshift/tasks || true
+
+# Run the Pipeline
 tkn pipeline start operator-ci-pipeline \
   --param git_repo_url=$CERTIFIED_REPO \
   --param git_branch=$CERTIFIED_BRANCH \
