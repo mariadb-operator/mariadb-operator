f57a88e5493e6c0cd95449c0b16bcf777928eaba
Martin Montes <martin11lrx@gmail.com>
2024-05-06 19:14:10 +0200

    SCC discovery

--- pkg/discovery/discovery.go
+++ pkg/discovery/discovery.go
@@ -3,14 +3,18 @@ package discovery
 import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 type DiscoveryClient struct {
 	discovery.DiscoveryClient
 }
 
-func NewDiscoveryClient(config *rest.Config) (*DiscoveryClient, error) {
+func NewDiscoveryClient() (*DiscoveryClient, error) {
+	config, err := ctrl.GetConfig()
+	if err != nil {
+		return nil, err
+	}
 	discoveryClient, err := discovery.NewDiscoveryClientForConfig(config)
 	if err != nil {
 		return nil, err
@@ -24,8 +28,12 @@ func (c *DiscoveryClient) ServiceMonitorExist() (bool, error) {
 	return c.resourceExist("monitoring.coreos.com/v1", "servicemonitors")
 }
 
+func (c *DiscoveryClient) SecurityContextConstrainstsExist() (bool, error) {
+	return c.resourceExist("security.openshift.io/v1", "securitycontextconstraints")
+}
+
 func (c *DiscoveryClient) resourceExist(groupVersion, kind string) (bool, error) {
-	apiResourceList, err := c.ServerResourcesForGroupVersion(groupVersion)
+	apiResourceList, err := c.DiscoveryClient.ServerResourcesForGroupVersion(groupVersion)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
 			return false, nil
