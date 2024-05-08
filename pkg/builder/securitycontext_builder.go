37680f5e47a37c6fd460fb5e9b5088ade02389fa
Martin Montes <martin11lrx@gmail.com>
2024-05-08 11:01:32 +0200

    Added method to build PodSecurityContext

--- pkg/builder/securitycontext_builder.go
+++ pkg/builder/securitycontext_builder.go
@@ -19,3 +19,17 @@ func (b *Builder) buildContainerSecurityContext(securityContext *corev1.Security
 	}
 	return securityContext, nil
 }
+
+func (b *Builder) buildPodSecurityContext(podSecurityContext *corev1.PodSecurityContext) (*corev1.PodSecurityContext, error) {
+	sccExists, err := b.discovery.SecurityContextConstrainstsExist()
+	if err != nil {
+		return nil, fmt.Errorf("error discovering SecurityContextConstraints: %v", err)
+	}
+	// Delegate SecurityContext assigment to OpenShift.
+	// A SecurityContext is created based on SecurityContextConstraints.
+	// See: https://redhat-connect.gitbook.io/certified-operator-guide/troubleshooting-and-resources/sccs
+	if sccExists {
+		return nil, nil
+	}
+	return podSecurityContext, nil
+}
--- pkg/builder/securitycontext_builder_test.go
+++ pkg/builder/securitycontext_builder_test.go
@@ -9,14 +9,14 @@ import (
 	"k8s.io/utils/ptr"
 )
 
-func TestContainerSecurityBuilder(t *testing.T) {
+func TestBuildContainerSecurityContext(t *testing.T) {
 	builder := newTestBuilder(t)
 
 	sc, err := builder.buildContainerSecurityContext(&corev1.SecurityContext{
 		RunAsUser: ptr.To(int64(999)),
 	})
 	if err != nil {
-		t.Fatalf("unexpected error building Security Context: %v", err)
+		t.Fatalf("unexpected error building SecurityContext: %v", err)
 	}
 	if sc == nil {
 		t.Error("SecurityContext must be non nil")
@@ -40,9 +40,47 @@ func TestContainerSecurityBuilder(t *testing.T) {
 		RunAsUser: ptr.To(int64(999)),
 	})
 	if err != nil {
-		t.Fatalf("unexpected error building Security Context: %v", err)
+		t.Fatalf("unexpected error building SecurityContext: %v", err)
 	}
 	if sc != nil {
 		t.Error("SecurityContext must be nil")
+	}
+}
+
+func TestBuildPodSecurityContext(t *testing.T) {
+	builder := newTestBuilder(t)
+
+	sc, err := builder.buildPodSecurityContext(&corev1.PodSecurityContext{
+		RunAsUser: ptr.To(int64(999)),
+	})
+	if err != nil {
+		t.Fatalf("unexpected error building PodSecurityContext: %v", err)
+	}
+	if sc == nil {
+		t.Error("PodSecurityContext must be non nil")
+	}
+
+	resource := &metav1.APIResourceList{
+		GroupVersion: "security.openshift.io/v1",
+		APIResources: []metav1.APIResource{
+			{
+				Name: "securitycontextconstraints",
+			},
+		},
+	}
+	discovery, err := discovery.NewFakeDiscovery(resource)
+	if err != nil {
+		t.Fatalf("unexpected error getting discovery: %v", err)
+	}
+	builder = newTestBuilder(t, WithDiscovery(discovery))
+
+	sc, err = builder.buildPodSecurityContext(&corev1.PodSecurityContext{
+		RunAsUser: ptr.To(int64(999)),
+	})
+	if err != nil {
+		t.Fatalf("unexpected error building PodSecurityContext: %v", err)
+	}
+	if sc != nil {
+		t.Error("PodSecurityContext must be nil")
 	}
 }
