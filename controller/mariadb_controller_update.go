dd72df454f8c1cd72ebd773625051be08dd9ecb5
Martin Montes <martin11lrx@gmail.com>
2024-05-22 19:23:51 +0200

    Updating replicas first and primary lst

--- controller/mariadb_controller_update.go
+++ controller/mariadb_controller_update.go
@@ -11,9 +11,11 @@ import (
 	mariadbv1alpha1 "github.com/mariadb-operator/mariadb-operator/api/v1alpha1"
 	labels "github.com/mariadb-operator/mariadb-operator/pkg/builder/labels"
 	podpkg "github.com/mariadb-operator/mariadb-operator/pkg/pod"
+	"github.com/mariadb-operator/mariadb-operator/pkg/wait"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	klabels "k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -51,7 +53,56 @@ func (r *MariaDBReconciler) reconcileUpdates(ctx context.Context, mdb *mariadbv1
 		logger.V(1).Info("Waiting for all Pods to be ready to proceed with the update. Requeuing...")
 		return ctrl.Result{RequeueAfter: 1 * time.Second}, nil
 	}
+
+	for _, replicaPod := range podsByRole.replicas {
+		if podpkg.PodUpdated(&replicaPod, stsUpdateRevision) {
+			logger.V(1).Info("Replica Pod up to date", "pod", replicaPod.Name)
+			continue
+		}
+		logger.Info("Updating replica Pod", "pod", replicaPod.Name)
+		if err := r.updatePod(ctx, &replicaPod, stsUpdateRevision, logger); err != nil {
+			return ctrl.Result{}, fmt.Errorf("error updating replica Pod '%s': %v", replicaPod.Name, err)
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	primaryPod := podsByRole.primary
+	if podpkg.PodUpdated(&primaryPod, stsUpdateRevision) {
+		logger.V(1).Info("Primary Pod up to date", "pod", primaryPod.Name)
+		return ctrl.Result{}, nil
+	}
+	logger.Info("Updating primary Pod", "pod", primaryPod.Name)
+	if err := r.updatePod(ctx, &primaryPod, stsUpdateRevision, logger); err != nil {
+		return ctrl.Result{}, fmt.Errorf("error updating primary Pod '%s': %v", primaryPod.Name, err)
+	}
 	return ctrl.Result{}, nil
+}
+
+func (r *MariaDBReconciler) updatePod(ctx context.Context, pod *corev1.Pod, updateRevision string, logger logr.Logger) error {
+	if err := r.Delete(ctx, pod); err != nil {
+		return fmt.Errorf("error deleting Pod '%s': %v", pod.Name, err)
+	}
+
+	updateCtx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+	if err := r.pollUntilPodUpdated(updateCtx, client.ObjectKeyFromObject(pod), updateRevision, logger); err != nil {
+		return fmt.Errorf("error waiting for primary Pod '%s' to be updated: %v", pod.Name, err)
+	}
+	return nil
+}
+
+func (r *MariaDBReconciler) pollUntilPodUpdated(ctx context.Context, podKey types.NamespacedName, updateRevision string,
+	logger logr.Logger) error {
+	return wait.PollUntilSucessWithTimeout(ctx, logger, func(ctx context.Context) error {
+		var pod corev1.Pod
+		if err := r.Get(ctx, podKey, &pod); err != nil {
+			return fmt.Errorf("error getting Pod '%s': %v", podKey.Name, err)
+		}
+		if podpkg.PodUpdated(&pod, updateRevision) {
+			return nil
+		}
+		return errors.New("Pod stale")
+	})
 }
 
 type podRoleSet struct {
