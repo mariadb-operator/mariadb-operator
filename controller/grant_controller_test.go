c8f3b81d36ae5daa7f2019ce6037eabbef7ee28c
Martin Montes <martin11lrx@gmail.com>
2024-05-25 22:19:35 +0200

    Refactor tests to be more readable

--- controller/grant_controller_test.go
+++ controller/grant_controller_test.go
@@ -12,226 +12,222 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
-var _ = Describe("Grant controller", func() {
-	Context("When creating a Grant for all tables and databases", func() {
-		It("Should reconcile", func() {
-			By("Creating a User")
-			userKey := types.NamespacedName{
-				Name:      "grant-user-all-test",
-				Namespace: testNamespace,
-			}
-			user := mariadbv1alpha1.User{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      userKey.Name,
-					Namespace: userKey.Namespace,
+var _ = Describe("Grant", func() {
+	It("should grant privileges for all tables and databases", func() {
+		By("Creating a User")
+		userKey := types.NamespacedName{
+			Name:      "grant-user-all-test",
+			Namespace: testNamespace,
+		}
+		user := mariadbv1alpha1.User{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      userKey.Name,
+				Namespace: userKey.Namespace,
+			},
+			Spec: mariadbv1alpha1.UserSpec{
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
+					},
+					WaitForIt: true,
 				},
-				Spec: mariadbv1alpha1.UserSpec{
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
+				PasswordSecretKeyRef: &corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: testPwdKey.Name,
 					},
-					PasswordSecretKeyRef: &corev1.SecretKeySelector{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: testPwdKey.Name,
-						},
-						Key: testPwdSecretKey,
-					},
-					MaxUserConnections: 20,
+					Key: testPwdSecretKey,
 				},
-			}
-			Expect(k8sClient.Create(testCtx, &user)).To(Succeed())
-			DeferCleanup(func() {
-				Expect(k8sClient.Delete(testCtx, &user)).To(Succeed())
-			})
-
-			By("Expecting User to be ready eventually")
-			Eventually(func() bool {
-				if err := k8sClient.Get(testCtx, userKey, &user); err != nil {
-					return false
-				}
-				return user.IsReady()
-			}, testTimeout, testInterval).Should(BeTrue())
-
-			By("Creating a Grant")
-			grantKey := types.NamespacedName{
-				Name:      "grant-select-insert-update-test",
-				Namespace: testNamespace,
-			}
-			grant := mariadbv1alpha1.Grant{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      grantKey.Name,
-					Namespace: grantKey.Namespace,
-				},
-				Spec: mariadbv1alpha1.GrantSpec{
-					SQLTemplate: mariadbv1alpha1.SQLTemplate{
-						RetryInterval: &metav1.Duration{Duration: 1 * time.Second},
-					},
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
-					},
-					Privileges: []string{
-						"SELECT",
-						"INSERT",
-						"UPDATE",
-					},
-					Database:    "*",
-					Table:       "*",
-					Username:    userKey.Name,
-					GrantOption: true,
-				},
-			}
-			Expect(k8sClient.Create(testCtx, &grant)).To(Succeed())
-			DeferCleanup(func() {
-				Expect(k8sClient.Delete(testCtx, &grant)).To(Succeed())
-			})
-
-			By("Expecting Grant to be ready eventually")
-			Eventually(func() bool {
-				if err := k8sClient.Get(testCtx, grantKey, &grant); err != nil {
-					return false
-				}
-				return grant.IsReady()
-			}, testTimeout, testInterval).Should(BeTrue())
-
-			By("Expecting Grant to eventually have finalizer")
-			Eventually(func() bool {
-				if err := k8sClient.Get(testCtx, grantKey, &grant); err != nil {
-					return false
-				}
-				return controllerutil.ContainsFinalizer(&grant, grantFinalizerName)
-			}, testTimeout, testInterval).Should(BeTrue())
+				MaxUserConnections: 20,
+			},
+		}
+		Expect(k8sClient.Create(testCtx, &user)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(k8sClient.Delete(testCtx, &user)).To(Succeed())
 		})
+
+		By("Expecting User to be ready eventually")
+		Eventually(func() bool {
+			if err := k8sClient.Get(testCtx, userKey, &user); err != nil {
+				return false
+			}
+			return user.IsReady()
+		}, testTimeout, testInterval).Should(BeTrue())
+
+		By("Creating a Grant")
+		grantKey := types.NamespacedName{
+			Name:      "grant-select-insert-update-test",
+			Namespace: testNamespace,
+		}
+		grant := mariadbv1alpha1.Grant{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      grantKey.Name,
+				Namespace: grantKey.Namespace,
+			},
+			Spec: mariadbv1alpha1.GrantSpec{
+				SQLTemplate: mariadbv1alpha1.SQLTemplate{
+					RetryInterval: &metav1.Duration{Duration: 1 * time.Second},
+				},
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
+					},
+					WaitForIt: true,
+				},
+				Privileges: []string{
+					"SELECT",
+					"INSERT",
+					"UPDATE",
+				},
+				Database:    "*",
+				Table:       "*",
+				Username:    userKey.Name,
+				GrantOption: true,
+			},
+		}
+		Expect(k8sClient.Create(testCtx, &grant)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(k8sClient.Delete(testCtx, &grant)).To(Succeed())
+		})
+
+		By("Expecting Grant to be ready eventually")
+		Eventually(func() bool {
+			if err := k8sClient.Get(testCtx, grantKey, &grant); err != nil {
+				return false
+			}
+			return grant.IsReady()
+		}, testTimeout, testInterval).Should(BeTrue())
+
+		By("Expecting Grant to eventually have finalizer")
+		Eventually(func() bool {
+			if err := k8sClient.Get(testCtx, grantKey, &grant); err != nil {
+				return false
+			}
+			return controllerutil.ContainsFinalizer(&grant, grantFinalizerName)
+		}, testTimeout, testInterval).Should(BeTrue())
 	})
 
-	Context("When creating a Grant for a database", func() {
-		It("Should reconcile", func() {
-			By("Creating a Database")
-			databaseKey := types.NamespacedName{
-				Name:      "grant-database-test",
-				Namespace: testNamespace,
-			}
-			database := mariadbv1alpha1.Database{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      databaseKey.Name,
-					Namespace: databaseKey.Namespace,
-				},
-				Spec: mariadbv1alpha1.DatabaseSpec{
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
+	It("should grant privileges for a database", func() {
+		By("Creating a Database")
+		databaseKey := types.NamespacedName{
+			Name:      "grant-database-test",
+			Namespace: testNamespace,
+		}
+		database := mariadbv1alpha1.Database{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      databaseKey.Name,
+				Namespace: databaseKey.Namespace,
+			},
+			Spec: mariadbv1alpha1.DatabaseSpec{
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
 					},
-					CharacterSet: "utf8",
-					Collate:      "utf8_general_ci",
+					WaitForIt: true,
 				},
-			}
-			Expect(k8sClient.Create(testCtx, &database)).To(Succeed())
-			DeferCleanup(func() {
-				Expect(k8sClient.Delete(testCtx, &database)).To(Succeed())
-			})
-
-			By("Expecting Database to be ready eventually")
-			Eventually(func() bool {
-				if err := k8sClient.Get(testCtx, databaseKey, &database); err != nil {
-					return false
-				}
-				return database.IsReady()
-			}, testTimeout, testInterval).Should(BeTrue())
-
-			By("Creating a User")
-			userKey := types.NamespacedName{
-				Name:      "grant-user-database-test",
-				Namespace: testNamespace,
-			}
-			user := mariadbv1alpha1.User{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      userKey.Name,
-					Namespace: userKey.Namespace,
-				},
-				Spec: mariadbv1alpha1.UserSpec{
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
-					},
-					PasswordSecretKeyRef: &corev1.SecretKeySelector{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: testPwdKey.Name,
-						},
-						Key: testPwdSecretKey,
-					},
-					MaxUserConnections: 20,
-				},
-			}
-			Expect(k8sClient.Create(testCtx, &user)).To(Succeed())
-			DeferCleanup(func() {
-				Expect(k8sClient.Delete(testCtx, &user)).To(Succeed())
-			})
-
-			By("Expecting User to be ready eventually")
-			Eventually(func() bool {
-				if err := k8sClient.Get(testCtx, userKey, &user); err != nil {
-					return false
-				}
-				return user.IsReady()
-			}, testTimeout, testInterval).Should(BeTrue())
-
-			By("Creating a Grant")
-			grantKey := types.NamespacedName{
-				Name:      "grant-all-test",
-				Namespace: testNamespace,
-			}
-			grant := mariadbv1alpha1.Grant{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      grantKey.Name,
-					Namespace: grantKey.Namespace,
-				},
-				Spec: mariadbv1alpha1.GrantSpec{
-					SQLTemplate: mariadbv1alpha1.SQLTemplate{
-						RetryInterval: &metav1.Duration{Duration: 1 * time.Second},
-					},
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
-					},
-					Privileges: []string{
-						"ALL",
-					},
-					Database:    testDatabase,
-					Table:       "*",
-					Username:    userKey.Name,
-					GrantOption: true,
-				},
-			}
-			Expect(k8sClient.Create(testCtx, &grant)).To(Succeed())
-			DeferCleanup(func() {
-				Expect(k8sClient.Delete(testCtx, &grant)).To(Succeed())
-			})
-
-			By("Expecting Grant to be ready eventually")
-			Eventually(func() bool {
-				if err := k8sClient.Get(testCtx, grantKey, &grant); err != nil {
-					return false
-				}
-				return grant.IsReady()
-			}, testTimeout, testInterval).Should(BeTrue())
-
-			By("Expecting Grant to eventually have finalizer")
-			Eventually(func() bool {
-				if err := k8sClient.Get(testCtx, grantKey, &grant); err != nil {
-					return false
-				}
-				return controllerutil.ContainsFinalizer(&grant, grantFinalizerName)
-			}, testTimeout, testInterval).Should(BeTrue())
+				CharacterSet: "utf8",
+				Collate:      "utf8_general_ci",
+			},
+		}
+		Expect(k8sClient.Create(testCtx, &database)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(k8sClient.Delete(testCtx, &database)).To(Succeed())
 		})
+
+		By("Expecting Database to be ready eventually")
+		Eventually(func() bool {
+			if err := k8sClient.Get(testCtx, databaseKey, &database); err != nil {
+				return false
+			}
+			return database.IsReady()
+		}, testTimeout, testInterval).Should(BeTrue())
+
+		By("Creating a User")
+		userKey := types.NamespacedName{
+			Name:      "grant-user-database-test",
+			Namespace: testNamespace,
+		}
+		user := mariadbv1alpha1.User{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      userKey.Name,
+				Namespace: userKey.Namespace,
+			},
+			Spec: mariadbv1alpha1.UserSpec{
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
+					},
+					WaitForIt: true,
+				},
+				PasswordSecretKeyRef: &corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: testPwdKey.Name,
+					},
+					Key: testPwdSecretKey,
+				},
+				MaxUserConnections: 20,
+			},
+		}
+		Expect(k8sClient.Create(testCtx, &user)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(k8sClient.Delete(testCtx, &user)).To(Succeed())
+		})
+
+		By("Expecting User to be ready eventually")
+		Eventually(func() bool {
+			if err := k8sClient.Get(testCtx, userKey, &user); err != nil {
+				return false
+			}
+			return user.IsReady()
+		}, testTimeout, testInterval).Should(BeTrue())
+
+		By("Creating a Grant")
+		grantKey := types.NamespacedName{
+			Name:      "grant-all-test",
+			Namespace: testNamespace,
+		}
+		grant := mariadbv1alpha1.Grant{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      grantKey.Name,
+				Namespace: grantKey.Namespace,
+			},
+			Spec: mariadbv1alpha1.GrantSpec{
+				SQLTemplate: mariadbv1alpha1.SQLTemplate{
+					RetryInterval: &metav1.Duration{Duration: 1 * time.Second},
+				},
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
+					},
+					WaitForIt: true,
+				},
+				Privileges: []string{
+					"ALL",
+				},
+				Database:    testDatabase,
+				Table:       "*",
+				Username:    userKey.Name,
+				GrantOption: true,
+			},
+		}
+		Expect(k8sClient.Create(testCtx, &grant)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(k8sClient.Delete(testCtx, &grant)).To(Succeed())
+		})
+
+		By("Expecting Grant to be ready eventually")
+		Eventually(func() bool {
+			if err := k8sClient.Get(testCtx, grantKey, &grant); err != nil {
+				return false
+			}
+			return grant.IsReady()
+		}, testTimeout, testInterval).Should(BeTrue())
+
+		By("Expecting Grant to eventually have finalizer")
+		Eventually(func() bool {
+			if err := k8sClient.Get(testCtx, grantKey, &grant); err != nil {
+				return false
+			}
+			return controllerutil.ContainsFinalizer(&grant, grantFinalizerName)
+		}, testTimeout, testInterval).Should(BeTrue())
 	})
 })
--- controller/sqljob_controller_test.go
+++ controller/sqljob_controller_test.go
@@ -10,235 +10,231 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ = Describe("SqlJob controller", func() {
-	Context("When creating SqlJobs", func() {
-		It("Should reconcile", func() {
-			createUsersJob := mariadbv1alpha1.SqlJob{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "sqljob-01-create-table-users",
-					Namespace: testNamespace,
+var _ = Describe("SqlJob", func() {
+	It("should reconcile a Job", func() {
+		createUsersJob := mariadbv1alpha1.SqlJob{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "sqljob-01-create-table-users",
+				Namespace: testNamespace,
+			},
+			Spec: mariadbv1alpha1.SqlJobSpec{
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
+					},
+					WaitForIt: true,
 				},
-				Spec: mariadbv1alpha1.SqlJobSpec{
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
+				InheritMetadata: &mariadbv1alpha1.Metadata{
+					Labels: map[string]string{
+						"k8s.mariadb.com/test": "test",
 					},
-					InheritMetadata: &mariadbv1alpha1.Metadata{
-						Labels: map[string]string{
-							"k8s.mariadb.com/test": "test",
-						},
-						Annotations: map[string]string{
-							"k8s.mariadb.com/test": "test",
-						},
+					Annotations: map[string]string{
+						"k8s.mariadb.com/test": "test",
 					},
-					Username: testUser,
-					PasswordSecretKeyRef: corev1.SecretKeySelector{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: testPwdKey.Name,
-						},
-						Key: testPwdSecretKey,
+				},
+				Username: testUser,
+				PasswordSecretKeyRef: corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: testPwdKey.Name,
 					},
-					Database: &testDatabase,
-					Sql: func() *string {
-						sql := `CREATE TABLE IF NOT EXISTS users (
+					Key: testPwdSecretKey,
+				},
+				Database: &testDatabase,
+				Sql: func() *string {
+					sql := `CREATE TABLE IF NOT EXISTS users (
 							id bigint PRIMARY KEY AUTO_INCREMENT,
 							username varchar(255) NOT NULL,
 							email varchar(255) NOT NULL,
 							UNIQUE KEY name__unique_idx (username),
 							UNIQUE KEY email__unique_idx (email)
 						);`
-						return &sql
-					}(),
-				},
-			}
+					return &sql
+				}(),
+			},
+		}
 
-			insertUsersJob := mariadbv1alpha1.SqlJob{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "sqljob-02-1-insert-users",
-					Namespace: testNamespace,
+		insertUsersJob := mariadbv1alpha1.SqlJob{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "sqljob-02-1-insert-users",
+				Namespace: testNamespace,
+			},
+			Spec: mariadbv1alpha1.SqlJobSpec{
+				DependsOn: []corev1.LocalObjectReference{
+					{
+						Name: createUsersJob.Name,
+					},
 				},
-				Spec: mariadbv1alpha1.SqlJobSpec{
-					DependsOn: []corev1.LocalObjectReference{
-						{
-							Name: createUsersJob.Name,
-						},
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
 					},
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
+					WaitForIt: true,
+				},
+				InheritMetadata: &mariadbv1alpha1.Metadata{
+					Labels: map[string]string{
+						"k8s.mariadb.com/test": "test",
 					},
-					InheritMetadata: &mariadbv1alpha1.Metadata{
-						Labels: map[string]string{
-							"k8s.mariadb.com/test": "test",
-						},
-						Annotations: map[string]string{
-							"k8s.mariadb.com/test": "test",
-						},
+					Annotations: map[string]string{
+						"k8s.mariadb.com/test": "test",
 					},
-					Username: testUser,
-					PasswordSecretKeyRef: corev1.SecretKeySelector{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: testPwdKey.Name,
-						},
-						Key: testPwdSecretKey,
+				},
+				Username: testUser,
+				PasswordSecretKeyRef: corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: testPwdKey.Name,
 					},
-					Database: &testDatabase,
-					Sql: func() *string {
-						sql := `INSERT INTO users(username, email) VALUES('mmontes11','mariadb-operator@proton.me') 
+					Key: testPwdSecretKey,
+				},
+				Database: &testDatabase,
+				Sql: func() *string {
+					sql := `INSERT INTO users(username, email) VALUES('mmontes11','mariadb-operator@proton.me') 
 						ON DUPLICATE KEY UPDATE username='mmontes11';`
-						return &sql
-					}(),
-				},
-			}
+					return &sql
+				}(),
+			},
+		}
 
-			createReposJob := mariadbv1alpha1.SqlJob{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "sqljob-02-2-create-table-repos",
-					Namespace: testNamespace,
+		createReposJob := mariadbv1alpha1.SqlJob{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "sqljob-02-2-create-table-repos",
+				Namespace: testNamespace,
+			},
+			Spec: mariadbv1alpha1.SqlJobSpec{
+				DependsOn: []corev1.LocalObjectReference{
+					{
+						Name: createUsersJob.Name,
+					},
 				},
-				Spec: mariadbv1alpha1.SqlJobSpec{
-					DependsOn: []corev1.LocalObjectReference{
-						{
-							Name: createUsersJob.Name,
-						},
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
 					},
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
+					WaitForIt: true,
+				},
+				InheritMetadata: &mariadbv1alpha1.Metadata{
+					Labels: map[string]string{
+						"k8s.mariadb.com/test": "test",
 					},
-					InheritMetadata: &mariadbv1alpha1.Metadata{
-						Labels: map[string]string{
-							"k8s.mariadb.com/test": "test",
-						},
-						Annotations: map[string]string{
-							"k8s.mariadb.com/test": "test",
-						},
+					Annotations: map[string]string{
+						"k8s.mariadb.com/test": "test",
 					},
-					Username: testUser,
-					PasswordSecretKeyRef: corev1.SecretKeySelector{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: testPwdKey.Name,
-						},
-						Key: testPwdSecretKey,
+				},
+				Username: testUser,
+				PasswordSecretKeyRef: corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: testPwdKey.Name,
 					},
-					Database: &testDatabase,
-					Sql: func() *string {
-						sql := `CREATE TABLE IF NOT EXISTS repos (
+					Key: testPwdSecretKey,
+				},
+				Database: &testDatabase,
+				Sql: func() *string {
+					sql := `CREATE TABLE IF NOT EXISTS repos (
 							id bigint PRIMARY KEY AUTO_INCREMENT,
 							name varchar(255) NOT NULL,
 							owner_id bigint NOT NULL,
 							UNIQUE KEY name__unique_idx (name),
 							FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 						);`
-						return &sql
-					}(),
-				},
-			}
-			sqlJobs := []mariadbv1alpha1.SqlJob{
-				createUsersJob,
-				insertUsersJob,
-				createReposJob,
-			}
+					return &sql
+				}(),
+			},
+		}
+		sqlJobs := []mariadbv1alpha1.SqlJob{
+			createUsersJob,
+			insertUsersJob,
+			createReposJob,
+		}
 
-			By("Creating SqlJobs")
-			for _, sqlJob := range sqlJobs {
-				Expect(k8sClient.Create(testCtx, &sqlJob)).To(Succeed())
-				sqlJob := sqlJob
-				DeferCleanup(func() {
-					Expect(k8sClient.Delete(testCtx, &sqlJob)).To(Succeed())
-				})
-			}
+		By("Creating SqlJobs")
+		for _, sqlJob := range sqlJobs {
+			Expect(k8sClient.Create(testCtx, &sqlJob)).To(Succeed())
+			sqlJob := sqlJob
+			DeferCleanup(func() {
+				Expect(k8sClient.Delete(testCtx, &sqlJob)).To(Succeed())
+			})
+		}
 
-			By("Expecting SqlJobs to complete eventually")
-			for _, j := range sqlJobs {
-				Eventually(func() bool {
-					var sqlJob mariadbv1alpha1.SqlJob
-					if err := k8sClient.Get(testCtx, client.ObjectKeyFromObject(&j), &sqlJob); err != nil {
-						return false
-					}
-					return sqlJob.IsComplete()
-				}, testHighTimeout, testInterval).Should(BeTrue())
-			}
-
-			By("Expecting to create a Job")
-			for _, sj := range sqlJobs {
+		By("Expecting SqlJobs to complete eventually")
+		for _, j := range sqlJobs {
+			Eventually(func() bool {
 				var sqlJob mariadbv1alpha1.SqlJob
-				Expect(k8sClient.Get(testCtx, client.ObjectKeyFromObject(&sj), &sqlJob)).To(Succeed())
-				var job batchv1.Job
-				Expect(k8sClient.Get(testCtx, client.ObjectKeyFromObject(&sqlJob), &job)).To(Succeed())
+				if err := k8sClient.Get(testCtx, client.ObjectKeyFromObject(&j), &sqlJob); err != nil {
+					return false
+				}
+				return sqlJob.IsComplete()
+			}, testHighTimeout, testInterval).Should(BeTrue())
+		}
 
-				By("Expecting Jobs to have metadata")
-				Expect(job.ObjectMeta.Labels).NotTo(BeNil())
-				Expect(job.ObjectMeta.Labels).To(HaveKeyWithValue("k8s.mariadb.com/test", "test"))
-				Expect(job.ObjectMeta.Annotations).NotTo(BeNil())
-				Expect(job.ObjectMeta.Annotations).To(HaveKeyWithValue("k8s.mariadb.com/test", "test"))
+		By("Expecting to create a Job")
+		for _, sj := range sqlJobs {
+			var sqlJob mariadbv1alpha1.SqlJob
+			Expect(k8sClient.Get(testCtx, client.ObjectKeyFromObject(&sj), &sqlJob)).To(Succeed())
+			var job batchv1.Job
+			Expect(k8sClient.Get(testCtx, client.ObjectKeyFromObject(&sqlJob), &job)).To(Succeed())
 
-				By("Expecting to create a ServiceAccount")
-				var svcAcc corev1.ServiceAccount
-				key := sqlJob.Spec.JobPodTemplate.ServiceAccountKey(job.ObjectMeta)
-				Expect(k8sClient.Get(testCtx, key, &svcAcc)).To(Succeed())
+			By("Expecting Jobs to have metadata")
+			Expect(job.ObjectMeta.Labels).NotTo(BeNil())
+			Expect(job.ObjectMeta.Labels).To(HaveKeyWithValue("k8s.mariadb.com/test", "test"))
+			Expect(job.ObjectMeta.Annotations).NotTo(BeNil())
+			Expect(job.ObjectMeta.Annotations).To(HaveKeyWithValue("k8s.mariadb.com/test", "test"))
 
-				Expect(svcAcc.ObjectMeta.Labels).NotTo(BeNil())
-				Expect(svcAcc.ObjectMeta.Labels).To(HaveKeyWithValue("k8s.mariadb.com/test", "test"))
-				Expect(svcAcc.ObjectMeta.Annotations).NotTo(BeNil())
-				Expect(svcAcc.ObjectMeta.Annotations).To(HaveKeyWithValue("k8s.mariadb.com/test", "test"))
-			}
-		})
+			By("Expecting to create a ServiceAccount")
+			var svcAcc corev1.ServiceAccount
+			key := sqlJob.Spec.JobPodTemplate.ServiceAccountKey(job.ObjectMeta)
+			Expect(k8sClient.Get(testCtx, key, &svcAcc)).To(Succeed())
+
+			Expect(svcAcc.ObjectMeta.Labels).NotTo(BeNil())
+			Expect(svcAcc.ObjectMeta.Labels).To(HaveKeyWithValue("k8s.mariadb.com/test", "test"))
+			Expect(svcAcc.ObjectMeta.Annotations).NotTo(BeNil())
+			Expect(svcAcc.ObjectMeta.Annotations).To(HaveKeyWithValue("k8s.mariadb.com/test", "test"))
+		}
 	})
 
-	Context("When creating scheduled SqlJob", func() {
-		It("Should reconcile", func() {
-			scheduledSqlJob := mariadbv1alpha1.SqlJob{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "sqljob-scheduled",
-					Namespace: testNamespace,
+	It("should reconcile a CronJob", func() {
+		scheduledSqlJob := mariadbv1alpha1.SqlJob{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "sqljob-scheduled",
+				Namespace: testNamespace,
+			},
+			Spec: mariadbv1alpha1.SqlJobSpec{
+				Schedule: &mariadbv1alpha1.Schedule{
+					Cron: "*/1 * * * *",
 				},
-				Spec: mariadbv1alpha1.SqlJobSpec{
-					Schedule: &mariadbv1alpha1.Schedule{
-						Cron: "*/1 * * * *",
+				MariaDBRef: mariadbv1alpha1.MariaDBRef{
+					ObjectReference: corev1.ObjectReference{
+						Name: testMdbkey.Name,
 					},
-					MariaDBRef: mariadbv1alpha1.MariaDBRef{
-						ObjectReference: corev1.ObjectReference{
-							Name: testMdbkey.Name,
-						},
-						WaitForIt: true,
+					WaitForIt: true,
+				},
+				Username: testUser,
+				PasswordSecretKeyRef: corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: testPwdKey.Name,
 					},
-					Username: testUser,
-					PasswordSecretKeyRef: corev1.SecretKeySelector{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: testPwdKey.Name,
-						},
-						Key: testPwdSecretKey,
-					},
-					Database: &testDatabase,
-					Sql: func() *string {
-						sql := `CREATE TABLE IF NOT EXISTS orders (
+					Key: testPwdSecretKey,
+				},
+				Database: &testDatabase,
+				Sql: func() *string {
+					sql := `CREATE TABLE IF NOT EXISTS orders (
 							id bigint PRIMARY KEY AUTO_INCREMENT,
 							email varchar(255) NOT NULL,
 							UNIQUE KEY email__unique_idx (email)
 						);`
-						return &sql
-					}(),
-				},
-			}
+					return &sql
+				}(),
+			},
+		}
 
-			By("Creating SqlJob")
-			Expect(k8sClient.Create(testCtx, &scheduledSqlJob)).To(Succeed())
-			DeferCleanup(func() {
-				Expect(k8sClient.Delete(testCtx, &scheduledSqlJob)).To(Succeed())
-			})
-
-			By("Expecting to create a CronJob eventually")
-			Eventually(func() bool {
-				var cronJob batchv1.CronJob
-				return k8sClient.Get(testCtx, client.ObjectKeyFromObject(&scheduledSqlJob), &cronJob) != nil
-			}, testHighTimeout, testInterval).Should(BeTrue())
+		By("Creating SqlJob")
+		Expect(k8sClient.Create(testCtx, &scheduledSqlJob)).To(Succeed())
+		DeferCleanup(func() {
+			Expect(k8sClient.Delete(testCtx, &scheduledSqlJob)).To(Succeed())
 		})
+
+		By("Expecting to create a CronJob eventually")
+		Eventually(func() bool {
+			var cronJob batchv1.CronJob
+			return k8sClient.Get(testCtx, client.ObjectKeyFromObject(&scheduledSqlJob), &cronJob) != nil
+		}, testHighTimeout, testInterval).Should(BeTrue())
 	})
 })
