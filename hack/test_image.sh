a99a8c79eeb427315026f1163217989cb3c56176
Martin Montes <martin11lrx@gmail.com>
2024-05-27 19:05:06 +0200

    Added test image script

---
+++ hack/test_image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ -z "$GHA_TOKEN" ]; then 
+  echo "GHA_TOKEN environment variable is mandatory"
+  exit 1
+fi
+if [ -z "$MARIADB_IMAGE" ]; then 
+  echo "MARIADB_IMAGE environment variable is mandatory"
+  exit 1
+fi
+
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GHA_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/mariadb-operator/mariadb-operator/actions/workflows/test-image.yml/dispatches \
+  -d "{\"ref\":\"main\",\"inputs\":{\"mariadb_image\":\"$MARIADB_IMAGE\"}}"
