b64103e7fc443f14d25b8683eb669597a68d0d66
Martin Montes <martin11lrx@gmail.com>
2024-06-02 12:08:19 +0200

    Decrease timeout

--- controller/utils_test.go
+++ controller/utils_test.go
@@ -33,7 +33,7 @@ import (
 )
 
 var (
-	testVeryHighTimeout = 6 * time.Minute
+	testVeryHighTimeout = 5 * time.Minute
 	testHighTimeout     = 3 * time.Minute
 	testTimeout         = 1 * time.Minute
 	testInterval        = 1 * time.Second
