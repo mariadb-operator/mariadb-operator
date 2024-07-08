ab54a85e1c0fd3f65de7344a34807ee37819aa23
Martin Montes <martin11lrx@gmail.com>
2024-07-08 15:27:20 +0200

    Added docs targets

--- make/docs.mk
+++ make/docs.mk
@@ -1,9 +1,9 @@
 ##@ Documentation
 
-DOCS_IMG ?= jnorwood/helm-docs:v1.11.0
+DOCS_HELM_IMG ?= jnorwood/helm-docs:v1.11.0
 .PHONY: docs-helm
 docs-helm: ## Generate Helm chart docs.
-	docker run --rm -v $(shell pwd)/$(HELM_DIR):/helm-docs -u $(shell id -u) $(DOCS_IMG)
+	docker run --rm -v $(shell pwd)/$(HELM_DIR):/helm-docs -u $(shell id -u) $(DOCS_HELM_IMG)
 
 .PHONY: docs-api
 docs-api: crd-ref-docs ## Generate API reference docs.
@@ -18,6 +18,25 @@ docs-toc: mdtoc ## Generate table of contents in docs.
 	@for f in $$(ls docs/*.md | grep -v 'API_REFERENCE' | grep -v 'UPGRADE'); do \
 		$(MDTOC) --inplace $$f; \
 	done
+
+MKDOCS_IMG ?= squidfunk/mkdocs-material:9.5.27
+MKDOCS ?= docker run --rm -it \
+	-u $(shell id -u):$(shell id -g) \
+	-v $(shell pwd):/docs \
+	-p 8000:8000 \
+	$(MKDOCS_IMG)
+
+.PHONY: docs-new
+docs-new: ## Create new documentation site.
+	$(MKDOCS) new .
+
+.PHONY: docs-serve
+docs-serve: ## Serve documentation site locally for development.
+	$(MKDOCS)
+
+.PHONY: docs-build
+docs-build: ## Build documentation site.
+	$(MKDOCS) build
 
 .PHONY: docs
 docs: docs-helm docs-api docs-toc ## Generate docs.
