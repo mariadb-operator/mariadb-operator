457094bc153a6567f2078e2f09c3a2a9ac8f472a
mmontes11 <martin11lrx@gmail.com>
2024-07-30 22:12:16 +0200

    Minor improvements

--- pkg/reflect/reflect_test.go
+++ pkg/reflect/reflect_test.go
@@ -11,23 +11,23 @@ func TestIsNil(t *testing.T) {
 		input interface{}
 		want  bool
 	}{
-		{"nil interface", nil, true},
-		{"nil pointer", (*int)(nil), true},
-		{"nil slice", ([]int)(nil), true},
-		{"nil map", (map[string]int)(nil), true},
 		{"nil channel", (chan int)(nil), true},
 		{"nil func", (func())(nil), true},
+		{"nil interface containing nil pointer", interface{}((*int)(nil)), true},
+		{"nil interface", nil, true},
+		{"nil map", (map[string]int)(nil), true},
+		{"nil pointer", (*int)(nil), true},
+		{"nil slice", ([]int)(nil), true},
 		{"nil unsafe pointer", (unsafe.Pointer)(nil), true},
-		{"non-nil pointer", new(int), false},
-		{"non-nil slice", []int{}, false},
-		{"non-nil map", map[string]int{}, false},
 		{"non-nil channel", make(chan int), false},
 		{"non-nil func", func() {}, false},
-		{"non-nil unsafe pointer", unsafe.Pointer(new(int)), false},
-		{"non-nil interface", interface{}(0), false},
 		{"non-nil int", 123, false},
+		{"non-nil interface", interface{}(0), false},
+		{"non-nil map", map[string]int{}, false},
+		{"non-nil pointer", new(int), false},
+		{"non-nil slice", []int{}, false},
 		{"non-nil string", "hello", false},
-		{"nil interface containing nil pointer", interface{}((*int)(nil)), true},
+		{"non-nil unsafe pointer", unsafe.Pointer(new(int)), false},
 	}
 
 	for _, tt := range tests {
