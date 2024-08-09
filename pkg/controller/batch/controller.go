083c8b0cb9802a9aa197720fff4bea5cfc4a4f59
Marin Bezhanov <marin.bezhanov@gmail.com>
2024-08-09 11:25:04 +0300

    Add TimeZone spec to Backup and SqlJob resources

--- pkg/controller/batch/controller.go
+++ pkg/controller/batch/controller.go
@@ -140,6 +140,7 @@ func (r *BatchReconciler) reconcileCronJob(ctx context.Context, key types.Namesp
 	patch := client.MergeFrom(existingCronJob.DeepCopy())
 	existingCronJob.Spec.FailedJobsHistoryLimit = desiredCronJob.Spec.FailedJobsHistoryLimit
 	existingCronJob.Spec.SuccessfulJobsHistoryLimit = desiredCronJob.Spec.SuccessfulJobsHistoryLimit
+	existingCronJob.Spec.TimeZone = desiredCronJob.Spec.TimeZone
 	existingCronJob.Spec.Schedule = desiredCronJob.Spec.Schedule
 	existingCronJob.Spec.Suspend = desiredCronJob.Spec.Suspend
 	existingCronJob.Spec.JobTemplate.Spec.BackoffLimit = desiredCronJob.Spec.JobTemplate.Spec.BackoffLimit
