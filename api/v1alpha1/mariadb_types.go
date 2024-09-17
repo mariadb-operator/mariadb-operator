1430b1df8d1f8cebbb9a69202eb6a596d4631ee5
mmontes11 <martin11lrx@gmail.com>
2024-09-17 16:51:02 +0200

    Update docs

--- api/v1alpha1/mariadb_types.go
+++ api/v1alpha1/mariadb_types.go
@@ -322,7 +322,7 @@ type UpdateStrategy struct {
 	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	RollingUpdate *appsv1.RollingUpdateStatefulSetStrategy `json:"rollingUpdate,omitempty"`
 	// AutoUpdateDataPlane indicates whether the Galera data plane version (agent and init containers) should be automatically updated based on the operator version. It defaults to false.
-	// Updating the operator will trigger updates on all the MariaDB instances that have this flag set to true. This, it is recommended to progressively set this flag after having updated the operator.
+	// Updating the operator will trigger updates on all the MariaDB instances that have this flag set to true. Thus, it is recommended to progressively set this flag after having updated the operator.
 	// +optional
 	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	AutoUpdateDataPlane *bool `json:"autoUpdateDataPlane,omitempty"`
