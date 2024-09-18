a4c2d1de5ee59a34d91eb07138bdae2ff7ef0e64
mmontes11 <martin11lrx@gmail.com>
2024-09-18 14:39:30 +0200

    Support for currentNamespaceOnly in helm chart

--- internal/helmtest/helm_test.go
+++ internal/helmtest/helm_test.go
@@ -3,21 +3,23 @@ package test
 import (
 	"testing"
 
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-
 	. "github.com/onsi/gomega"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
-func TestHelmTemplatesIncludeExtraEnvFrom(t *testing.T) {
+var (
+	helmReleaseName = "mariadb-operator"
+	helmChartPath   = "../../deploy/charts/mariadb-operator"
+	testNamespace   = "test"
+)
 
-	helmChartPath := "../../deploy/charts/mariadb-operator"
-
+func TestHelmExtraEnvFrom(t *testing.T) {
 	RegisterTestingT(t)
-	options := &helm.Options{
-
+	opts := &helm.Options{
 		// see https://helm.sh/docs/intro/using_helm/#the-format-and-limitations-of---set
 		SetValues: map[string]string{
 			"extraEnvFrom[0].configMapRef.name": `env-extra-configmap`,
@@ -25,24 +27,161 @@ func TestHelmTemplatesIncludeExtraEnvFrom(t *testing.T) {
 		},
 	}
 
-	// Run RenderTemplate to render but only `templates/deployment.yaml`
-	output := helm.RenderTemplate(t, options, helmChartPath, "mariadb-operator", []string{"templates/deployment.yaml"})
-
-	// Unmarshal for assertions
+	deploymentData := helm.RenderTemplate(t, opts, helmChartPath, helmReleaseName, []string{"templates/deployment.yaml"})
 	var deployment appsv1.Deployment
-	helm.UnmarshalK8SYaml(t, output, &deployment)
+	helm.UnmarshalK8SYaml(t, deploymentData, &deployment)
 
 	container := deployment.Spec.Template.Spec.Containers[0]
 	Expect(container.Name).To(Equal("controller"))
 
-	cmSource := corev1.EnvFromSource{}
-	cmSource.ConfigMapRef = &corev1.ConfigMapEnvSource{}
-	cmSource.ConfigMapRef.Name = "env-extra-configmap"
-	Expect(container.EnvFrom).To(ContainElement(cmSource))
+	configMapEnvFrom := corev1.EnvFromSource{
+		ConfigMapRef: &corev1.ConfigMapEnvSource{
+			LocalObjectReference: corev1.LocalObjectReference{
+				Name: "env-extra-configmap",
+			},
+		},
+	}
+	Expect(container.EnvFrom).To(ContainElement(configMapEnvFrom))
 
-	secretSource := corev1.EnvFromSource{}
-	secretSource.SecretRef = &corev1.SecretEnvSource{}
-	secretSource.SecretRef.Name = "env-extra-secret"
-	Expect(container.EnvFrom).To(ContainElement(secretSource))
+	secretEnvFrom := corev1.EnvFromSource{
+		SecretRef: &corev1.SecretEnvSource{
+			LocalObjectReference: corev1.LocalObjectReference{
+				Name: "env-extra-secret",
+			},
+		},
+	}
+	Expect(container.EnvFrom).To(ContainElement(secretEnvFrom))
+}
 
+func TestHelmCurrentNamespaceOnly(t *testing.T) {
+	RegisterTestingT(t)
+	opts := &helm.Options{
+		SetValues: map[string]string{
+			"currentNamespaceOnly": `true`,
+		},
+		KubectlOptions: &k8s.KubectlOptions{
+			Namespace: testNamespace,
+		},
+	}
+
+	deploymentData := helm.RenderTemplate(t, opts, helmChartPath, helmReleaseName, []string{"templates/deployment.yaml"})
+	var deployment appsv1.Deployment
+	helm.UnmarshalK8SYaml(t, deploymentData, &deployment)
+
+	container := deployment.Spec.Template.Spec.Containers[0]
+	Expect(container.Name).To(Equal("controller"))
+
+	env := corev1.EnvVar{
+		Name:  "WATCH_NAMESPACE",
+		Value: testNamespace,
+	}
+	Expect(container.Env).To(ContainElement(env))
+
+	expectedTemplates := []string{
+		"templates/rbac-namespace.yaml",
+	}
+	unexpectedTemplates := []string{
+		"templates/cert-controller-deployment.yaml",
+		"templates/cert-controller-rbac.yaml",
+		"templates/cert-controller-serviceaccount.yaml",
+		"templates/cert-controller-servicemonitor.yaml",
+		"templates/rbac-user.yaml",
+		"templates/rbac.yaml",
+		"templates/webhook-certificate.yaml",
+		"templates/webhook-config.yaml",
+		"templates/webhook-deployment.yaml",
+		"templates/webhook-secret.yaml",
+		"templates/webhook-service.yaml",
+		"templates/webhook-serviceaccount.yaml",
+		"templates/webhook-servicemonitor.yaml",
+	}
+	testHelmTemplates(t, opts, expectedTemplates, unexpectedTemplates)
+}
+
+func TestHelmClusterWide(t *testing.T) {
+	RegisterTestingT(t)
+	opts := &helm.Options{
+		SetValues: map[string]string{
+			"webhook.cert.certManager.enabled": `true`,
+			"metrics.enabled":                  `true`,
+		},
+		KubectlOptions: &k8s.KubectlOptions{
+			Namespace: testNamespace,
+		},
+	}
+
+	deploymentData := helm.RenderTemplate(t, opts, helmChartPath, helmReleaseName, []string{"templates/deployment.yaml"})
+	var deployment appsv1.Deployment
+	helm.UnmarshalK8SYaml(t, deploymentData, &deployment)
+
+	container := deployment.Spec.Template.Spec.Containers[0]
+	Expect(container.Name).To(Equal("controller"))
+
+	env := corev1.EnvVar{
+		Name:  "WATCH_NAMESPACE",
+		Value: testNamespace,
+	}
+	Expect(container.Env).ToNot(ContainElement(env))
+
+	expectedTemplates := []string{
+		"templates/rbac-user.yaml",
+		"templates/rbac.yaml",
+		"templates/webhook-certificate.yaml",
+		"templates/webhook-config.yaml",
+		"templates/webhook-deployment.yaml",
+		"templates/webhook-secret.yaml",
+		"templates/webhook-service.yaml",
+		"templates/webhook-serviceaccount.yaml",
+	}
+	unexpectedTemplates := []string{
+		"templates/rbac-namespace.yaml",
+	}
+	testHelmTemplates(t, opts, expectedTemplates, unexpectedTemplates)
+}
+
+func TestHelmCertManager(t *testing.T) {
+	RegisterTestingT(t)
+	opts := &helm.Options{
+		SetValues: map[string]string{
+			"webhook.cert.certManager.enabled": `true`,
+		},
+	}
+
+	expectedTemplates := []string{
+		"templates/webhook-certificate.yaml",
+		"templates/webhook-secret.yaml",
+	}
+	unexpectedTemplates := []string{
+		"templates/cert-controller-deployment.yaml",
+		"templates/cert-controller-rbac.yaml",
+		"templates/cert-controller-serviceaccount.yaml",
+	}
+	testHelmTemplates(t, opts, expectedTemplates, unexpectedTemplates)
+}
+
+func TestHelmMetrics(t *testing.T) {
+	RegisterTestingT(t)
+	opts := &helm.Options{
+		SetValues: map[string]string{
+			"metrics.enabled": `true`,
+		},
+	}
+
+	expectedTemplates := []string{
+		"templates/cert-controller-servicemonitor.yaml",
+		"templates/webhook-servicemonitor.yaml",
+	}
+	testHelmTemplates(t, opts, expectedTemplates, nil)
+}
+
+func testHelmTemplates(t *testing.T, opts *helm.Options, expectedTemplates, unexpectedTemplates []string) {
+	for _, tpl := range expectedTemplates {
+		_, err := helm.RenderTemplateE(t, opts, helmChartPath, helmReleaseName, []string{tpl})
+		Expect(err).ToNot(HaveOccurred())
+	}
+
+	for _, tpl := range unexpectedTemplates {
+		_, err := helm.RenderTemplateE(t, opts, helmChartPath, helmReleaseName, []string{tpl})
+		Expect(err).To(HaveOccurred())
+	}
 }
