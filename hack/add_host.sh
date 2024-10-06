c10bfe92b09508bb6e5fc43a66ab1527d94c4aaa
Artem Babii <arthem.babij@gmail.com>
2024-10-06 09:53:20 +0300

    use printf to add hosts to /etc/hosts

--- hack/add_host.sh
+++ hack/add_host.sh
@@ -10,5 +10,5 @@ if grep -q "^$IP\s*$HOSTNAME" /etc/hosts; then
   echo "\"$HOSTNAME\" host already exists in /etc/hosts"
 else
   echo "Adding \"$HOSTNAME\" to /etc/hosts";
-  sudo -- sh -c -e "echo '# mariadb-operator\n$IP\t$HOSTNAME' >> /etc/hosts";
+  sudo -- sh -c -e "printf '# mariadb-operator\n%s\t%s\n' '$IP' '$HOSTNAME' >> /etc/hosts";
 fi
