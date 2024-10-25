ce3b4cf904ae9c47f03baeb3e432b0eb4dbb7c6a
mmontes11 <martin11lrx@gmail.com>
2024-10-25 16:18:09 +0200

    Disabled replication test

--- internal/controller/mariadb_controller_replication_test.go
+++ internal/controller/mariadb_controller_replication_test.go
@@ -208,7 +208,7 @@ var _ = Describe("MariaDB replication", Ordered, func() {
 	})
 
 	It("should fail and switch over primary", func() {
-		// TODO: re-evaluate this test when productionizing replication. See https://github.com/mariadb-operator/mariadb-operator/issues/738
+		Skip("TODO: re-evaluate this test when productionizing replication. See https://github.com/mariadb-operator/mariadb-operator/issues/738")
 
 		By("Expecting MariaDB primary to be set")
 		Eventually(func() bool {
