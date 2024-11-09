a617f898e73ee44a3fe3844984aab1b34569c84b
Martin Montes <martin11lrx@gmail.com>
2024-11-09 21:44:16 +0100

    Refactor local CAs

--- make/pki.mk
+++ make/pki.mk
@@ -5,51 +5,62 @@ CA_DIR ?= $(PKI_DIR)/ca
 CERT_SIZE ?= 4096
 
 .PHONY: ca
-ca: ca-server ca-client ## Generates CA keypairs.
+ca: ca-server ca-client ca-minio ## Generates CA keypairs.
 
-CA_SERVER_SECRET_NAME ?= mariadb-server-ca
-CA_SERVER_SECRET_NAMESPACE ?= default
-CA_SERVER_CERT ?= $(CA_DIR)/server.crt
-CA_SERVER_KEY ?= $(CA_DIR)/server.key
-CA_SERVER_SUBJECT ?= "/CN=mariadb-server-ca"
+CA_SECRET_NAME ?=
+CA_SECRET_NAMESPACE ?=
+CA_CERT ?=
+CA_KEY ?=
+CA_SUBJECT ?= 
+.PHONY: ca-root
+ca-root: ## Generates a self-signed root CA keypair.
+	@if [ ! -f "$(CA_CERT)" ] || [ ! -f "$(CA_KEY)" ]; then \
+		mkdir -p $(CA_DIR); \
+		openssl req -new -newkey rsa:$(CERT_SIZE) -x509 -sha256 -days 365 -nodes \
+			-out $(CA_CERT) -keyout $(CA_KEY) \
+			-subj $(CA_SUBJECT); \
+	else \
+		echo "CA files already exist, skipping generation."; \
+	fi
+
+	CERT_SECRET_NAME=$(CA_SECRET_NAME) \
+	CERT_SECRET_NAMESPACE=$(CA_SECRET_NAMESPACE) \
+	CERT=$(CA_CERT) \
+	KEY=$(CA_KEY) \
+	$(MAKE) cert-secret-tls
+
+CA_SERVER_CERT ?= $(CA_DIR)/server.crt 
+CA_SERVER_KEY ?= $(CA_DIR)/server.key 
 .PHONY: ca-server
 ca-server: ## Generates server CA keypair.
-	@if [ ! -f "$(CA_SERVER_CERT)" ] || [ ! -f "$(CA_SERVER_KEY)" ]; then \
-		mkdir -p $(CA_DIR); \
-		openssl req -new -newkey rsa:$(CERT_SIZE) -x509 -sha256 -days 365 -nodes \
-			-out $(CA_SERVER_CERT) -keyout $(CA_SERVER_KEY) \
-			-subj $(CA_SERVER_SUBJECT); \
-	else \
-		echo "CA server files already exist, skipping generation."; \
-	fi
+	CA_SECRET_NAME=mariadb-server-ca \
+	CA_SECRET_NAMESPACE=default \
+	CA_CERT=$(CA_SERVER_CERT) \
+	CA_KEY=$(CA_SERVER_KEY) \
+	CA_SUBJECT="/CN=mariadb-server-ca" \
+	$(MAKE) ca-root
 
-	CERT_SECRET_NAME=$(CA_SERVER_SECRET_NAME) \
-	CERT_SECRET_NAMESPACE=$(CA_SERVER_SECRET_NAMESPACE) \
-	CERT=$(CA_SERVER_CERT) \
-	KEY=$(CA_SERVER_KEY) \
-	$(MAKE) cert-secret-tls
-
-CA_CLIENT_SECRET_NAME ?= mariadb-client-ca
-CA_CLIENT_SECRET_NAMESPACE ?= default
-CA_CLIENT_CERT ?= $(CA_DIR)/client.crt
-CA_CLIENT_KEY ?= $(CA_DIR)/client.key
-CA_CLIENT_SUBJECT ?= "/CN=mariadb-client-ca"
+CA_CLIENT_CERT ?= $(CA_DIR)/client.crt 
+CA_CLIENT_KEY ?= $(CA_DIR)/client.key 
 .PHONY: ca-client
 ca-client: ## Generates client CA keypair.
-	@if [ ! -f "$(CA_CLIENT_CERT)" ] || [ ! -f "$(CA_CLIENT_KEY)" ]; then \
-		mkdir -p $(CA_DIR); \
-		openssl req -new -newkey rsa:$(CERT_SIZE) -x509 -sha256 -days 365 -nodes \
-			-out $(CA_CLIENT_CERT) -keyout $(CA_CLIENT_KEY) \
-			-subj $(CA_CLIENT_SUBJECT); \
-	else \
-		echo "CA client files already exist, skipping generation."; \
-	fi
+	CA_SECRET_NAME=mariadb-client-ca \
+	CA_SECRET_NAMESPACE=default \
+	CA_CERT=$(CA_CLIENT_CERT) \
+	CA_KEY=$(CA_CLIENT_KEY) \
+	CA_SUBJECT="/CN=mariadb-client-ca" \
+	$(MAKE) ca-root
 
-	CERT_SECRET_NAME=$(CA_CLIENT_SECRET_NAME) \
-	CERT_SECRET_NAMESPACE=$(CA_CLIENT_SECRET_NAMESPACE) \
-	CERT=$(CA_CLIENT_CERT) \
-	KEY=$(CA_CLIENT_KEY) \
-	$(MAKE) cert-secret-tls
+CA_MINIO_CERT ?= $(CA_DIR)/minio.crt 
+CA_MINIO_KEY ?= $(CA_DIR)/minio.key 
+.PHONY: ca-minio
+ca-minio: ## Generates minio CA keypair.
+	CA_SECRET_NAME=minio-ca \
+	CA_SECRET_NAMESPACE=default \
+	CA_CERT=$(CA_MINIO_CERT) \
+	CA_KEY=$(CA_MINIO_KEY) \
+	CA_SUBJECT="/CN=minio-ca" \
+	$(MAKE) ca-root
 
 CERT_SECRET_NAME ?=
 CERT_SECRET_NAMESPACE ?=
@@ -90,16 +101,12 @@ cert-leaf-client: ## Generates leaf certificate keypair for a client.
 
 MARIADB_NAME ?= mariadb
 MARIADB_NAMESPACE ?= default
-MARIADB_SERVER_CERT ?= $(PKI_DIR)/$(MARIADB_NAME)-server.crt
-MARIADB_SERVER_KEY ?= $(PKI_DIR)/$(MARIADB_NAME)-server.key
-MARIADB_CLIENT_CERT ?= $(PKI_DIR)/$(MARIADB_NAME)-client.crt
-MARIADB_CLIENT_KEY ?= $(PKI_DIR)/$(MARIADB_NAME)-client.key
 .PHONY: cert-leaf-mariadb
 cert-leaf-mariadb: ca ## Generate leaf certificates for MariaDB.
 	CERT_SECRET_NAME=$(MARIADB_NAME)-server-tls \
 	CERT_SECRET_NAMESPACE=$(MARIADB_NAMESPACE) \
-	CERT=$(MARIADB_SERVER_CERT) \
-	KEY=$(MARIADB_SERVER_KEY) \
+	CERT=$(PKI_DIR)/$(MARIADB_NAME)-server.crt \
+	KEY=$(PKI_DIR)/$(MARIADB_NAME)-server.key \
 	CERT_SUBJECT="/CN=$(MARIADB_NAME).default.svc.cluster.local" \
 	CERT_ALT_NAMES="$(CERT_ALT_NAMES)" \
 	CA_CERT=$(CA_SERVER_CERT) \
@@ -108,8 +115,8 @@ cert-leaf-mariadb: ca ## Generate leaf certificates for MariaDB.
 
 	CERT_SECRET_NAME=$(MARIADB_NAME)-client-tls \
 	CERT_SECRET_NAMESPACE=$(MARIADB_NAMESPACE) \
-	CERT=$(MARIADB_CLIENT_CERT) \
-	KEY=$(MARIADB_CLIENT_KEY) \
+	CERT=$(PKI_DIR)/$(MARIADB_NAME)-client.crt \
+	KEY=$(PKI_DIR)/$(MARIADB_NAME)-client.key \
 	CERT_SUBJECT="/CN=$(MARIADB_NAME)-client" \
 	CA_CERT=$(CA_CLIENT_CERT) \
 	CA_KEY=$(CA_CLIENT_KEY) \
@@ -131,7 +138,7 @@ WEBHOOK_PKI_DIR ?= /tmp/k8s-webhook-server/serving-certs
 .PHONY: cert-webhook
 cert-webhook: ca ## Generates webhook private key and certificate for local development.
 	PKI_DIR=$(WEBHOOK_PKI_DIR) \
-	CERT_SECRET_NAME=webhook \
+	CERT_SECRET_NAME=webhook-tls \
 	CERT_SECRET_NAMESPACE=default \
 	CA_CERT=$(CA_SERVER_CERT) \
 	CA_KEY=$(CA_SERVER_KEY) \
@@ -142,28 +149,15 @@ cert-webhook: ca ## Generates webhook private key and certificate for local deve
 	$(MAKE) cert-leaf
 
 MINIO_PKI_DIR ?= /tmp/pki-minio
-MINIO_CA_SECRET_NAME ?= minio-ca
-MINIO_CA_NAMESPACE ?= default
-MINIO_CERT_SECRET_NAME ?= minio-tls
-MINIO_CERT_NAMESPACE ?= minio
-MINIO_CERT ?= $(MINIO_PKI_DIR)/tls.crt
-MINIO_KEY ?= $(MINIO_PKI_DIR)/tls.key
-
 .PHONY: cert-minio
-cert-minio: ca kubectl ## Generates minio private key and certificate for local development.
-	CERT_SECRET_NAME=$(MINIO_CA_SECRET_NAME) \
-	CERT_SECRET_NAMESPACE=$(MINIO_CA_NAMESPACE) \
-	CERT=$(CA_SERVER_CERT) \
-	KEY=$(CA_SERVER_KEY) \
-	$(MAKE) cert-secret
-	
+cert-minio: ca ## Generates minio private key and certificate for local development.
 	PKI_DIR=$(MINIO_PKI_DIR) \
-	CERT_SECRET_NAME=$(MINIO_CERT_SECRET_NAME) \
-	CERT_SECRET_NAMESPACE=$(MINIO_CERT_NAMESPACE) \
-	CA_CERT=$(CA_SERVER_CERT) \
-	CA_KEY=$(CA_SERVER_KEY) \
-	CERT=$(MINIO_CERT) \
-	KEY=$(MINIO_KEY) \
+	CERT_SECRET_NAME=minio-tls \
+	CERT_SECRET_NAMESPACE=minio \
+	CA_CERT=$(CA_MINIO_CERT) \
+	CA_KEY=$(CA_MINIO_KEY) \
+	CERT=$(MINIO_PKI_DIR)/tls.crt \
+	KEY=$(MINIO_PKI_DIR)/tls.key \
 	CERT_SUBJECT="/CN=minio.minio.svc.cluster.local" \
 	CERT_ALT_NAMES="subjectAltName=DNS:minio,DNS:minio.minio,DNS:minio.minio.svc.cluster.local" \
 	$(MAKE) cert-leaf
