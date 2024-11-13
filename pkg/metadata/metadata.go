ec341d1b05f962a9adc3d41883af19d89ee0027b
mmontes11 <martin11lrx@gmail.com>
2024-11-13 17:51:00 +0100

    Trigger update when Galera config file changes

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -3,9 +3,12 @@ package metadata
 var (
 	WatchLabel = "k8s.mariadb.com/watch"
 
-	ReplicationAnnotation   = "k8s.mariadb.com/replication"
-	GaleraAnnotation        = "k8s.mariadb.com/galera"
-	MariadbAnnotation       = "k8s.mariadb.com/mariadb"
-	ConfigAnnotation        = "k8s.mariadb.com/config"
+	ReplicationAnnotation = "k8s.mariadb.com/replication"
+	GaleraAnnotation      = "k8s.mariadb.com/galera"
+	MariadbAnnotation     = "k8s.mariadb.com/mariadb"
+
+	ConfigAnnotation       = "k8s.mariadb.com/config"
+	ConfigGaleraAnnotation = "k8s.mariadb.com/config-galera"
+
 	WebhookConfigAnnotation = "k8s.mariadb.com/webhook"
 )
