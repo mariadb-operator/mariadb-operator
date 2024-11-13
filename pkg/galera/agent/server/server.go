87c4d0096c64ef9dc603dbfc510335d00ec45641
mmontes11 <martin11lrx@gmail.com>
2024-11-13 19:49:18 +0100

    Support for TLS in Galera agent

--- pkg/galera/agent/server/server.go
+++ pkg/galera/agent/server/server.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -20,13 +23,42 @@ func WithGracefulShutdownTimeout(timeout time.Duration) Option {
 	}
 }
 
+func WithTLSEnabled(tlsEnabled bool) Option {
+	return func(s *Server) {
+		s.tlsEnabled = tlsEnabled
+	}
+}
+
+func WithTLSCAPath(tlsCACertPath string) Option {
+	return func(s *Server) {
+		s.tlsCACertPath = tlsCACertPath
+	}
+}
+
+func WithTLSCertPath(tlsCertPath string) Option {
+	return func(s *Server) {
+		s.tlsCertPath = tlsCertPath
+	}
+}
+
+func WithTLSKeyPath(tlsKeyPath string) Option {
+	return func(s *Server) {
+		s.tlsKeyPath = tlsKeyPath
+	}
+}
+
 type Server struct {
 	httpServer              *http.Server
 	logger                  *logr.Logger
 	gracefulShutdownTimeout time.Duration
+
+	tlsEnabled    bool
+	tlsCACertPath string
+	tlsCertPath   string
+	tlsKeyPath    string
 }
 
-func NewServer(addr string, handler http.Handler, logger *logr.Logger, opts ...Option) *Server {
+func NewServer(addr string, handler http.Handler, logger *logr.Logger, opts ...Option) (*Server, error) {
 	srv := &Server{
 		httpServer: &http.Server{
 			Addr:    addr,
@@ -38,7 +70,16 @@ func NewServer(addr string, handler http.Handler, logger *logr.Logger, opts ...O
 	for _, setOpt := range opts {
 		setOpt(srv)
 	}
-	return srv
+
+	if srv.tlsEnabled {
+		srv.logger.Info("Configuring TLS")
+		tlsConfig, err := srv.getTLSConfig()
+		if err != nil {
+			return nil, fmt.Errorf("error getting TLS config: %v", err)
+		}
+		srv.httpServer.TLSConfig = tlsConfig
+	}
+	return srv, nil
 }
 
 func (s *Server) Start(ctx context.Context) error {
@@ -66,8 +107,16 @@ func (s *Server) Start(ctx context.Context) error {
 	}()
 
 	go func() {
-		s.logger.Info("Server listening", "addr", s.httpServer.Addr)
-		if err := s.httpServer.ListenAndServe(); err != http.ErrServerClosed {
+		logger := s.logger.WithValues("addr", s.httpServer.Addr, "tls", s.tlsEnabled)
+		listenFn := func() error {
+			if s.tlsEnabled {
+				return s.httpServer.ListenAndServeTLS("", "")
+			}
+			return s.httpServer.ListenAndServe()
+		}
+
+		logger.Info("Server listening")
+		if err := listenFn(); err != http.ErrServerClosed {
 			errChan <- fmt.Errorf("Error starting server: %v", err)
 		}
 	}()
@@ -78,4 +127,29 @@ func (s *Server) Start(ctx context.Context) error {
 	case err := <-errChan:
 		return err
 	}
+}
+
+func (s *Server) getTLSConfig() (*tls.Config, error) {
+	if !s.tlsEnabled {
+		return nil, errors.New("TLS must be enabled")
+	}
+	caCert, err := os.ReadFile(s.tlsCACertPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading CA cert: %v", err)
+	}
+	caCertPool := x509.NewCertPool()
+	if ok := caCertPool.AppendCertsFromPEM(caCert); !ok {
+		return nil, errors.New("unable to add CA cert to pool")
+	}
+
+	cert, err := tls.LoadX509KeyPair(s.tlsCertPath, s.tlsKeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("error loading x509 keypair: %v", err)
+	}
+
+	return &tls.Config{
+		ClientCAs:    caCertPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{cert},
+	}, nil
 }
