e671acc0f70900f8d0db63a43843eb90d71558b6
mmontes11 <martin11lrx@gmail.com>
2024-12-06 14:48:04 +0100

    Skip expired certs in CA bundle

--- pkg/pki/pem_test.go
+++ pkg/pki/pem_test.go
@@ -3,21 +3,21 @@ package pki
 import (
 	"testing"
 
-	"github.com/go-logr/logr"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestBundleCertificatePEMs(t *testing.T) {
-	logger := logr.Logger{}
 	tests := []struct {
 		name      string
 		pems      [][]byte
+		opts      []BundleOption
 		wantBytes []byte
 		wantErr   bool
 	}{
 		{
 			name:      "nil",
 			pems:      nil,
+			opts:      nil,
 			wantBytes: nil,
 			wantErr:   true,
 		},
@@ -27,6 +27,7 @@ func TestBundleCertificatePEMs(t *testing.T) {
 				[]byte(""),
 				[]byte(""),
 			},
+			opts:      nil,
 			wantBytes: nil,
 			wantErr:   true,
 		},
@@ -35,6 +36,7 @@ func TestBundleCertificatePEMs(t *testing.T) {
 			pems: [][]byte{
 				[]byte("test"),
 			},
+			opts:      nil,
 			wantBytes: nil,
 			wantErr:   true,
 		},
@@ -46,6 +48,7 @@ FOO
 -----END CERTIFICATE-----
 `),
 			},
+			opts:      nil,
 			wantBytes: nil,
 			wantErr:   true,
 		},
@@ -71,6 +74,7 @@ WVQiuKIOhYk=
 `),
 				[]byte("invalid data"),
 			},
+			opts: nil,
 			// Subject: CN=mariadb-client-ca
 			// Serial Number: a8:a4:f4:36:0a:dd:95:97:65:8a:b0:60:4d:f5:d1:47:ae:a9:6a
 			wantBytes: []byte(`-----BEGIN CERTIFICATE-----
@@ -111,6 +115,7 @@ WVQiuKIOhYk=
 -----END CERTIFICATE-----
 `),
 			},
+			opts: nil,
 			// Subject: CN=mariadb-client-ca
 			// Serial Number: a8:a4:f4:36:0a:dd:95:97:65:8a:b0:60:4d:f5:d1:47:ae:a9:6a
 			wantBytes: []byte(`-----BEGIN CERTIFICATE-----
@@ -168,6 +173,7 @@ idE60fGmuV8=
 -----END CERTIFICATE-----
 `),
 			},
+			opts: nil,
 			// Subject: CN=mariadb-client-ca
 			// Serial Number: a8:a4:f4:36:0a:dd:95:97:65:8a:b0:60:4d:f5:d1:47:ae:a9:6a
 			// Subject: CN=mariadb-server-ca
@@ -273,6 +279,7 @@ IOz3lKAeaG0=
 -----END CERTIFICATE-----
 `),
 			},
+			opts: nil,
 			// Subject: CN=mariadb-client-ca
 			// Serial Number: a8:a4:f4:36:0a:dd:95:97:65:8a:b0:60:4d:f5:d1:47:ae:a9:6a
 			// Subject: CN=mariadb-client-ca
@@ -410,6 +417,7 @@ idE60fGmuV8=
 -----END CERTIFICATE-----
 `),
 			},
+			opts: nil,
 			// Subject: CN=mariadb-client-ca
 			// Serial Number: a8:a4:f4:36:0a:dd:95:97:65:8a:b0:60:4d:f5:d1:47:ae:a9:6a
 			// Subject: CN=mariadb-server-ca
@@ -445,11 +453,54 @@ idE60fGmuV8=
 `),
 			wantErr: false,
 		},
+		{
+			name: "expired cert",
+			pems: [][]byte{
+				// Expired cert
+				[]byte(`-----BEGIN CERTIFICATE-----
+MIIFSzCCBDOgAwIBAgIQSueVSfqavj8QDxekeOFpCTANBgkqhkiG9w0BAQsFADCB
+kDELMAkGA1UEBhMCR0IxGzAZBgNVBAgTEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4G
+A1UEBxMHU2FsZm9yZDEaMBgGA1UEChMRQ09NT0RPIENBIExpbWl0ZWQxNjA0BgNV
+BAMTLUNPTU9ETyBSU0EgRG9tYWluIFZhbGlkYXRpb24gU2VjdXJlIFNlcnZlciBD
+QTAeFw0xNTA0MDkwMDAwMDBaFw0xNTA0MTIyMzU5NTlaMFkxITAfBgNVBAsTGERv
+bWFpbiBDb250cm9sIFZhbGlkYXRlZDEdMBsGA1UECxMUUG9zaXRpdmVTU0wgV2ls
+ZGNhcmQxFTATBgNVBAMUDCouYmFkc3NsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQAD
+ggEPADCCAQoCggEBAMIE7PiM7gTCs9hQ1XBYzJMY61yoaEmwIrX5lZ6xKyx2PmzA
+S2BMTOqytMAPgLaw+XLJhgL5XEFdEyt/ccRLvOmULlA3pmccYYz2QULFRtMWhyef
+dOsKnRFSJiFzbIRMeVXk0WvoBj1IFVKtsyjbqv9u/2CVSndrOfEk0TG23U3AxPxT
+uW1CrbV8/q71FdIzSOciccfCFHpsKOo3St/qbLVytH5aohbcabFXRNsKEqveww9H
+dFxBIuGa+RuT5q0iBikusbpJHAwnnqP7i/dAcgCskgjZjFeEU4EFy+b+a1SYQCeF
+xxC7c3DvaRhBB0VVfPlkPz0sw6l865MaTIbRyoUCAwEAAaOCAdUwggHRMB8GA1Ud
+IwQYMBaAFJCvajqUWgvYkOoSVnPfQ7Q6KNrnMB0GA1UdDgQWBBSd7sF7gQs6R2lx
+GH0RN5O8pRs/+zAOBgNVHQ8BAf8EBAMCBaAwDAYDVR0TAQH/BAIwADAdBgNVHSUE
+FjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwTwYDVR0gBEgwRjA6BgsrBgEEAbIxAQIC
+BzArMCkGCCsGAQUFBwIBFh1odHRwczovL3NlY3VyZS5jb21vZG8uY29tL0NQUzAI
+BgZngQwBAgEwVAYDVR0fBE0wSzBJoEegRYZDaHR0cDovL2NybC5jb21vZG9jYS5j
+b20vQ09NT0RPUlNBRG9tYWluVmFsaWRhdGlvblNlY3VyZVNlcnZlckNBLmNybDCB
+hQYIKwYBBQUHAQEEeTB3ME8GCCsGAQUFBzAChkNodHRwOi8vY3J0LmNvbW9kb2Nh
+LmNvbS9DT01PRE9SU0FEb21haW5WYWxpZGF0aW9uU2VjdXJlU2VydmVyQ0EuY3J0
+MCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5jb21vZG9jYS5jb20wIwYDVR0RBBww
+GoIMKi5iYWRzc2wuY29tggpiYWRzc2wuY29tMA0GCSqGSIb3DQEBCwUAA4IBAQBq
+evHa/wMHcnjFZqFPRkMOXxQhjHUa6zbgH6QQFezaMyV8O7UKxwE4PSf9WNnM6i1p
+OXy+l+8L1gtY54x/v7NMHfO3kICmNnwUW+wHLQI+G1tjWxWrAPofOxkt3+IjEBEH
+fnJ/4r+3ABuYLyw/zoWaJ4wQIghBK4o+gk783SHGVnRwpDTysUCeK1iiWQ8dSO/r
+ET7BSp68ZVVtxqPv1dSWzfGuJ/ekVxQ8lEEFeouhN0fX9X3c+s5vMaKwjOrMEpsi
+8TRwz311SotoKQwe6Zaoz7ASH1wq7mcvf71z81oBIgxw+s1F73hczg36TuHvzmWf
+RwxPuzZEaFZcVlmtqoq8
+-----END CERTIFICATE-----
+`),
+			},
+			opts: []BundleOption{
+				WithSkipExpired(true),
+			},
+			wantBytes: nil,
+			wantErr:   true,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			bundleBytes, err := BundleCertificatePEMs(logger, tt.pems...)
+			bundleBytes, err := BundleCertificatePEMs(tt.pems, tt.opts...)
 
 			if tt.wantErr && err == nil {
 				t.Error("expect error to have occurred, got nil")
