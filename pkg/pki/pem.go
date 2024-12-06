5c03bcdee0306dfae435a0963f88b56f5459f038
mmontes11 <martin11lrx@gmail.com>
2024-12-06 14:48:04 +0100

    Skip expired certs in CA bundle

--- pkg/pki/pem.go
+++ pkg/pki/pem.go
@@ -5,26 +5,52 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/go-logr/logr"
 )
 
-// PEMBlockType is a custom type representing the type of a PEM block.
-type PEMBlockType string
+// BundleOption represents a function that applies a bundle configuration.
+type BundleOption func(opts *BundleOptions)
 
-const (
-	// CertificatePEMBlockType represents the "CERTIFICATE" type for a PEM block.
-	CertificatePEMBlockType PEMBlockType = "CERTIFICATE"
-)
+// WithLogger sets the logger option.
+func WithLogger(logger logr.Logger) BundleOption {
+	return func(opts *BundleOptions) {
+		opts.logger = logger
+	}
+}
+
+// WithSkipExpired sets an option to skip expired certs.
+func WithSkipExpired(skipExpired bool) BundleOption {
+	return func(opts *BundleOptions) {
+		opts.skipExpired = skipExpired
+	}
+}
+
+// BundleOptions to be used with the bundle.
+type BundleOptions struct {
+	logger      logr.Logger
+	skipExpired bool
+}
+
+const certificatePEMBlockType string = "CERTIFICATE"
 
 // BundleCertificatePEMs bundles multiple PEM-encoded certificate slices into a single bundle.
-func BundleCertificatePEMs(logger logr.Logger, pems ...[]byte) ([]byte, error) {
+func BundleCertificatePEMs(pems [][]byte, bundleOpts ...BundleOption) ([]byte, error) {
+	opts := BundleOptions{
+		logger:      logr.Discard(),
+		skipExpired: false,
+	}
+	for _, opt := range bundleOpts {
+		opt(&opts)
+	}
+
 	var bundle []byte
 	var err error
 	existingCerts := make(map[string]struct{})
 
 	for _, pem := range pems {
-		bundle, err = appendPEM(bundle, pem, existingCerts, logger)
+		bundle, err = appendPEM(bundle, pem, existingCerts, opts)
 		if err != nil {
 			return nil, fmt.Errorf("error appending PEM: %v", err)
 		}
@@ -35,15 +61,15 @@ func BundleCertificatePEMs(logger logr.Logger, pems ...[]byte) ([]byte, error) {
 	return bundle, nil
 }
 
-func appendPEM(bundle []byte, pemBytes []byte, existingCerts map[string]struct{}, logger logr.Logger) ([]byte, error) {
+func appendPEM(bundle []byte, pemBytes []byte, existingCerts map[string]struct{}, opts BundleOptions) ([]byte, error) {
 	var block *pem.Block
 	for len(pemBytes) > 0 {
 		block, pemBytes = pem.Decode(pemBytes)
 		if block == nil {
-			logger.Error(errors.New("Invalid PEM block"), "Error decoding PEM block. Ignoring...")
+			opts.logger.Error(errors.New("Invalid PEM block"), "Error decoding PEM block. Ignoring...")
 			break
 		}
-		if block.Type != string(CertificatePEMBlockType) {
+		if block.Type != string(certificatePEMBlockType) {
 			return nil, fmt.Errorf("invalid PEM certificate block, got block type: %v", block.Type)
 		}
 
@@ -54,6 +80,14 @@ func appendPEM(bundle []byte, pemBytes []byte, existingCerts map[string]struct{}
 		certID := getCertID(cert)
 
 		if _, ok := existingCerts[certID]; ok {
+			opts.logger.V(1).Info("skipping existing certificate", "cert-id", certID)
+			continue
+		}
+
+		now := time.Now()
+		isExpired := now.Before(cert.NotBefore) || now.After(cert.NotAfter)
+		if opts.skipExpired && isExpired {
+			opts.logger.Info("skipping expired certificate", "cert-id", certID, "not-before", cert.NotBefore, "not-after", cert.NotAfter)
 			continue
 		}
 
