05fa64e5c40842023d9f76dbf33eb327621ab21c
mmontes11 <martin11lrx@gmail.com>
2024-12-20 15:30:47 +0100

    Added tests for keypair

--- pkg/pki/keypair_test.go
+++ pkg/pki/keypair_test.go
@@ -1,7 +1,11 @@
 package pki
 
 import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
 	"testing"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -135,43 +139,436 @@ nBZoGLkeDofSc+Ml4HRpi43U+fqhU77wr8Gq0YU74h7lFfiRI/M=
 `
 )
 
-func TestKeyPairFromTLSSecret(t *testing.T) {
-	secret := corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test",
+func TestNewKeyPair(t *testing.T) {
+	tests := []struct {
+		name    string
+		certPEM []byte
+		keyPEM  []byte
+		opts    []KeyPairOpt
+		wantErr bool
+	}{
+		{
+			name:    "Empty Cert PEM",
+			certPEM: []byte(""),
+			keyPEM: []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIAdp3iKnNA1kO2Ep5Hw7owMcm06SecFGdOqW/vO4k2AjoAoGCCqGSM49
+AwEHoUQDQgAEiTVhkriBksuWW5W3Mv9L918m1BECaHUl7ZV/Pz2q84wY9aEbxe2P
+J3c22DtEFzg9emNuruVS5/HL+hanzz4o+g==
+-----END EC PRIVATE KEY-----
+`),
+			opts:    []KeyPairOpt{WithSupportedPrivateKeys(PrivateKeyTypeECDSA)},
+			wantErr: true,
 		},
-		Type: corev1.SecretTypeTLS,
-		Data: map[string][]byte{
-			TLSCertKey: []byte(testTLSCert),
-			TLSKeyKey:  []byte(testTLSKey),
+		{
+			name: "Empty Key PEM",
+			certPEM: []byte(`-----BEGIN CERTIFICATE-----
+MIICXzCCAgagAwIBAgIRAIBgotjwHCDFrV2H9FWQrYIwCgYIKoZIzj0EAwIwNjEZ
+MBcGA1UEChMQbWFyaWFkYi1vcGVyYXRvcjEZMBcGA1UEAxMQbWFyaWFkYi1vcGVy
+YXRvcjAeFw0yNDEyMTkxNjI2NTVaFw0yNTEyMTkyMzI2NTVaMC8xLTArBgNVBAMT
+JG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2YzBZMBMGByqGSM49
+AgEGCCqGSM49AwEHA0IABIk1YZK4gZLLlluVtzL/S/dfJtQRAmh1Je2Vfz89qvOM
+GPWhG8Xtjyd3Ntg7RBc4PXpjbq7lUufxy/oWp88+KPqjgfswgfgwDgYDVR0PAQH/
+BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMAwGA1UdEwEB/wQCMAAwHwYDVR0j
+BBgwFoAUXZzsgcecviPCXlVdrBw/tUEC2uYwgaEGA1UdEQSBmTCBloIybWFyaWFk
+Yi1vcGVyYXRvci13ZWJob29rLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWyCJG1h
+cmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2Y4IgbWFyaWFkYi1vcGVy
+YXRvci13ZWJob29rLmRlZmF1bHSCGG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vazAK
+BggqhkjOPQQDAgNHADBEAiBSWY1rVufSE+3i0w553uJGJCC4Fpa6cvRPEti8X3Kp
+1AIgG0qN5IT9EsRZaY4J2vBYsbN5LL+qRI5N0XGYqVWXuD8=
+-----END CERTIFICATE-----			
+`),
+			keyPEM:  []byte(""),
+			opts:    []KeyPairOpt{WithSupportedPrivateKeys(PrivateKeyTypeECDSA)},
+			wantErr: true,
+		},
+		{
+			name:    "Invalid Cert PEM",
+			certPEM: []byte("invalid-cert"),
+			keyPEM: []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIAdp3iKnNA1kO2Ep5Hw7owMcm06SecFGdOqW/vO4k2AjoAoGCCqGSM49
+AwEHoUQDQgAEiTVhkriBksuWW5W3Mv9L918m1BECaHUl7ZV/Pz2q84wY9aEbxe2P
+J3c22DtEFzg9emNuruVS5/HL+hanzz4o+g==
+-----END EC PRIVATE KEY-----
+`),
+			opts:    []KeyPairOpt{WithSupportedPrivateKeys(PrivateKeyTypeECDSA)},
+			wantErr: true,
+		},
+		{
+			name: "Invalid Key PEM",
+			certPEM: []byte(`-----BEGIN CERTIFICATE-----
+MIICXzCCAgagAwIBAgIRAIBgotjwHCDFrV2H9FWQrYIwCgYIKoZIzj0EAwIwNjEZ
+MBcGA1UEChMQbWFyaWFkYi1vcGVyYXRvcjEZMBcGA1UEAxMQbWFyaWFkYi1vcGVy
+YXRvcjAeFw0yNDEyMTkxNjI2NTVaFw0yNTEyMTkyMzI2NTVaMC8xLTArBgNVBAMT
+JG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2YzBZMBMGByqGSM49
+AgEGCCqGSM49AwEHA0IABIk1YZK4gZLLlluVtzL/S/dfJtQRAmh1Je2Vfz89qvOM
+GPWhG8Xtjyd3Ntg7RBc4PXpjbq7lUufxy/oWp88+KPqjgfswgfgwDgYDVR0PAQH/
+BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMAwGA1UdEwEB/wQCMAAwHwYDVR0j
+BBgwFoAUXZzsgcecviPCXlVdrBw/tUEC2uYwgaEGA1UdEQSBmTCBloIybWFyaWFk
+Yi1vcGVyYXRvci13ZWJob29rLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWyCJG1h
+cmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2Y4IgbWFyaWFkYi1vcGVy
+YXRvci13ZWJob29rLmRlZmF1bHSCGG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vazAK
+BggqhkjOPQQDAgNHADBEAiBSWY1rVufSE+3i0w553uJGJCC4Fpa6cvRPEti8X3Kp
+1AIgG0qN5IT9EsRZaY4J2vBYsbN5LL+qRI5N0XGYqVWXuD8=
+-----END CERTIFICATE-----			
+`),
+			keyPEM:  []byte("invalid-key"),
+			opts:    []KeyPairOpt{WithSupportedPrivateKeys(PrivateKeyTypeRSA)},
+			wantErr: true,
+		},
+		{
+			name: "Valid",
+			certPEM: []byte(`-----BEGIN CERTIFICATE-----
+MIICXzCCAgagAwIBAgIRAIBgotjwHCDFrV2H9FWQrYIwCgYIKoZIzj0EAwIwNjEZ
+MBcGA1UEChMQbWFyaWFkYi1vcGVyYXRvcjEZMBcGA1UEAxMQbWFyaWFkYi1vcGVy
+YXRvcjAeFw0yNDEyMTkxNjI2NTVaFw0yNTEyMTkyMzI2NTVaMC8xLTArBgNVBAMT
+JG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2YzBZMBMGByqGSM49
+AgEGCCqGSM49AwEHA0IABIk1YZK4gZLLlluVtzL/S/dfJtQRAmh1Je2Vfz89qvOM
+GPWhG8Xtjyd3Ntg7RBc4PXpjbq7lUufxy/oWp88+KPqjgfswgfgwDgYDVR0PAQH/
+BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMAwGA1UdEwEB/wQCMAAwHwYDVR0j
+BBgwFoAUXZzsgcecviPCXlVdrBw/tUEC2uYwgaEGA1UdEQSBmTCBloIybWFyaWFk
+Yi1vcGVyYXRvci13ZWJob29rLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWyCJG1h
+cmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2Y4IgbWFyaWFkYi1vcGVy
+YXRvci13ZWJob29rLmRlZmF1bHSCGG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vazAK
+BggqhkjOPQQDAgNHADBEAiBSWY1rVufSE+3i0w553uJGJCC4Fpa6cvRPEti8X3Kp
+1AIgG0qN5IT9EsRZaY4J2vBYsbN5LL+qRI5N0XGYqVWXuD8=
+-----END CERTIFICATE-----			
+`),
+			keyPEM: []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIAdp3iKnNA1kO2Ep5Hw7owMcm06SecFGdOqW/vO4k2AjoAoGCCqGSM49
+AwEHoUQDQgAEiTVhkriBksuWW5W3Mv9L918m1BECaHUl7ZV/Pz2q84wY9aEbxe2P
+J3c22DtEFzg9emNuruVS5/HL+hanzz4o+g==
+-----END EC PRIVATE KEY-----
+`),
+			opts:    []KeyPairOpt{WithSupportedPrivateKeys(PrivateKeyTypeECDSA)},
+			wantErr: false,
+		},
+		{
+			name: "Unmatched",
+			certPEM: []byte(`-----BEGIN CERTIFICATE-----
+MIICXzCCAgagAwIBAgIRAIBgotjwHCDFrV2H9FWQrYIwCgYIKoZIzj0EAwIwNjEZ
+MBcGA1UEChMQbWFyaWFkYi1vcGVyYXRvcjEZMBcGA1UEAxMQbWFyaWFkYi1vcGVy
+YXRvcjAeFw0yNDEyMTkxNjI2NTVaFw0yNTEyMTkyMzI2NTVaMC8xLTArBgNVBAMT
+JG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2YzBZMBMGByqGSM49
+AgEGCCqGSM49AwEHA0IABIk1YZK4gZLLlluVtzL/S/dfJtQRAmh1Je2Vfz89qvOM
+GPWhG8Xtjyd3Ntg7RBc4PXpjbq7lUufxy/oWp88+KPqjgfswgfgwDgYDVR0PAQH/
+BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMAwGA1UdEwEB/wQCMAAwHwYDVR0j
+BBgwFoAUXZzsgcecviPCXlVdrBw/tUEC2uYwgaEGA1UdEQSBmTCBloIybWFyaWFk
+Yi1vcGVyYXRvci13ZWJob29rLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWyCJG1h
+cmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2Y4IgbWFyaWFkYi1vcGVy
+YXRvci13ZWJob29rLmRlZmF1bHSCGG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vazAK
+BggqhkjOPQQDAgNHADBEAiBSWY1rVufSE+3i0w553uJGJCC4Fpa6cvRPEti8X3Kp
+1AIgG0qN5IT9EsRZaY4J2vBYsbN5LL+qRI5N0XGYqVWXuD8=
+-----END CERTIFICATE-----			
+`),
+			keyPEM: []byte(`-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEA1l202NMTln0/ngg4JXUJLJXvhSjjHimO22c47tHhWvnzhtnK
+CrH8cWBnnxO11os5PcNIUYTxn04mZPRs+p1YkE9DMlp9Lgy/38304rr4kjllVspv
+l9MdrelqbcDy520rgF/YObfMZvzeseH2F5UK386IXb1KYSmp8dn7RU2HvUf17Z/z
+1ScdvOS4xXPNjuAi28REA72vPbFwLbt+mQxBQ/Aal6BNH5RhNIOZ9m8fVsWn/e/4
+hZTa2Ib/pp/3j2D1UlJqBiAh4cBeI0QYbj/hN5+OpVUJA3+OsGzFOBhs7KfqMAP3
+KDTt7sTrPV03QKKqhDjh3LIzdZyEWHPMJesMawIDAQABAoIBAEw5tf0D0YtJrj17
+nrtzCngYOLuY9mnbTTknU09YwlGfX8Er4HQ9Jg8KwM4ILDjF+OzFbAnQxDpph62O
+XNIg8UUfaj2Vf73IOtJSYindYlZconRiN5w9LeiRf47XdYhlgXp8ml6rxLs6X9XR
+C7kG/n7m6garMK+sKQofAQJ7tzDOpzna/V+C3z7vwupmypjVmKuqFfDOITUhk06Q
+WNUbiBSRbVtuWJ4+xpg5cZbCSIzlziQFbaRjQ0bHVIoJ/DrHI8OCEELjcQi7aMPX
+Z6XZnpVteae5o5lpw1AbB7c4ALF5B4Z1RQKakb+mK7o4F6SV/TOf2Fb7Gwp5gSOO
+Udo/acECgYEA7woaxJvkyJH6G83kt73RUudFbKIUo/lv8E0BLcN/7jo8zVMQIoxN
+m6fffVWfy2zVd5rLus4wjq+f5jmAprJykkA7n3HIM6HJ2v9KJPVCBKA8bgJ5eU0Y
+c7VFz8O0Z8dj/p9oVrHLo0Aqo+69rXG6wcyck1au3FEw16jflU1MaMUCgYEA5ZNv
+fHHDtWDjCPM75N7aaw2C9ENL3fi8Fwh/h4ZRj/BftHnHZkjY3bpW1/17lXuTnbE6
+uHeq3s/wBt4+F/N61ps1a7NTOtZHst5fPZZUjWuT0vq0EN2KB81iR/N4ld7w3F3v
+bZSPNXpm1J3wSAVrL1tHdQdWXdkOTeTA4wAnE28CgYANZKuLSJDRDBzPYgHmqaQI
+2RxyscImTduPw0DFp6aLWof9mSHWTbYreoRzKVECvN5ZDTtNBDCETiLPa3lh3a29
+tAujK2TkP7RnqNYmq/c++xtnrovP2Bn+obF/qp95ERrxMU1PTjbytq2s8bt+9Fha
+c3RybPDvNz1dWADvBJ27YQKBgAF6b49XlDEIzK10E4CnxrRFxAAaptRpE5z6Wwfe
+X4wTuioJVrVb5rmWx5Rgd3lA8HRlfcFOU/VXVW5V5AR3duUG3tMwtmp8kr2eHPLi
+kuzOMod7QcmSA5+FPQrFkJM2ekqQ+Ee2Wy22+g6IbdGo50XIyq8AOxgjm6n4vR05
+FQdVAoGAUt0hi789SX+BKzkWWMnds8HZzO3a5OgN9on+Hd/BVaQG6+F4wBGyjdjz
+PEZhWvsvx9Qge+DhwW3vfTDH2RItevD5Av4x0jZX0TWPoII8aP07VmDQ4g0cEkKh
+nBZoGLkeDofSc+Ml4HRpi43U+fqhU77wr8Gq0YU74h7lFfiRI/M=
+-----END RSA PRIVATE KEY-----
+`),
+			opts:    []KeyPairOpt{WithSupportedPrivateKeys(PrivateKeyTypeRSA)},
+			wantErr: true,
+		},
+		{
+			name: "Unsupported",
+			certPEM: []byte(`-----BEGIN CERTIFICATE-----
+MIICXzCCAgagAwIBAgIRAIBgotjwHCDFrV2H9FWQrYIwCgYIKoZIzj0EAwIwNjEZ
+MBcGA1UEChMQbWFyaWFkYi1vcGVyYXRvcjEZMBcGA1UEAxMQbWFyaWFkYi1vcGVy
+YXRvcjAeFw0yNDEyMTkxNjI2NTVaFw0yNTEyMTkyMzI2NTVaMC8xLTArBgNVBAMT
+JG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2YzBZMBMGByqGSM49
+AgEGCCqGSM49AwEHA0IABIk1YZK4gZLLlluVtzL/S/dfJtQRAmh1Je2Vfz89qvOM
+GPWhG8Xtjyd3Ntg7RBc4PXpjbq7lUufxy/oWp88+KPqjgfswgfgwDgYDVR0PAQH/
+BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMAwGA1UdEwEB/wQCMAAwHwYDVR0j
+BBgwFoAUXZzsgcecviPCXlVdrBw/tUEC2uYwgaEGA1UdEQSBmTCBloIybWFyaWFk
+Yi1vcGVyYXRvci13ZWJob29rLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWyCJG1h
+cmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2Y4IgbWFyaWFkYi1vcGVy
+YXRvci13ZWJob29rLmRlZmF1bHSCGG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vazAK
+BggqhkjOPQQDAgNHADBEAiBSWY1rVufSE+3i0w553uJGJCC4Fpa6cvRPEti8X3Kp
+1AIgG0qN5IT9EsRZaY4J2vBYsbN5LL+qRI5N0XGYqVWXuD8=
+-----END CERTIFICATE-----			
+`),
+			keyPEM: []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIAdp3iKnNA1kO2Ep5Hw7owMcm06SecFGdOqW/vO4k2AjoAoGCCqGSM49
+AwEHoUQDQgAEiTVhkriBksuWW5W3Mv9L918m1BECaHUl7ZV/Pz2q84wY9aEbxe2P
+J3c22DtEFzg9emNuruVS5/HL+hanzz4o+g==
+-----END EC PRIVATE KEY-----
+`),
+			opts:    []KeyPairOpt{WithSupportedPrivateKeys(PrivateKeyTypeRSA)},
+			wantErr: true,
 		},
 	}
 
-	keyPair, err := NewKeyPairFromTLSSecret(&secret, WithSupportedPrivateKeys(PrivateKeyTypeRSA))
-	if err != nil {
-		t.Fatalf("Unexpected error creating KeyPair from TLS Secret: %v", err)
-	}
-	if keyPair == nil {
-		t.Fatal("KeyPair should nit be nul")
-	}
-	certs, err := keyPair.Certificates()
-	if err != nil {
-		t.Fatalf("error getting KeyPair certificates: %v", err)
-	}
-	if len(certs) == 0 {
-		t.Fatal("no certificates for keypair were found")
-	}
-	cert := certs[0]
-
-	expectedCN := "mariadb-operator-webhook.default.svc"
-	if expectedCN != cert.Subject.CommonName {
-		t.Fatalf("Expected CommonName to be %v. Got %v", expectedCN, cert.Subject.CommonName)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewKeyPair(tt.certPEM, tt.keyPEM, tt.opts...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewKeyPair() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
 
-func TestKeyPairInvalidPEM(t *testing.T) {
-	_, err := NewKeyPair([]byte("foo"), []byte("bar"))
-	if err == nil {
-		t.Fatal("Expected KeyPair creation to fail")
+func TestNewKeyPairFromTLSSecret(t *testing.T) {
+	tests := []struct {
+		name    string
+		secret  *corev1.Secret
+		wantErr bool
+	}{
+		{
+			name: "Empty TLS Secret",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Type: corev1.SecretTypeTLS,
+				Data: map[string][]byte{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid Secret Type",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Type: corev1.SecretTypeOpaque,
+				Data: map[string][]byte{
+					TLSCertKey: []byte(testTLSCert),
+					TLSKeyKey:  []byte(testTLSKey),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid Cert PEM",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Type: corev1.SecretTypeTLS,
+				Data: map[string][]byte{
+					TLSCertKey: []byte("invalid-cert"),
+					TLSKeyKey: []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIAdp3iKnNA1kO2Ep5Hw7owMcm06SecFGdOqW/vO4k2AjoAoGCCqGSM49
+AwEHoUQDQgAEiTVhkriBksuWW5W3Mv9L918m1BECaHUl7ZV/Pz2q84wY9aEbxe2P
+J3c22DtEFzg9emNuruVS5/HL+hanzz4o+g==
+-----END EC PRIVATE KEY-----
+`),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid Key PEM",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Type: corev1.SecretTypeTLS,
+				Data: map[string][]byte{
+					TLSCertKey: []byte(`-----BEGIN CERTIFICATE-----
+MIICXzCCAgagAwIBAgIRAIBgotjwHCDFrV2H9FWQrYIwCgYIKoZIzj0EAwIwNjEZ
+MBcGA1UEChMQbWFyaWFkYi1vcGVyYXRvcjEZMBcGA1UEAxMQbWFyaWFkYi1vcGVy
+YXRvcjAeFw0yNDEyMTkxNjI2NTVaFw0yNTEyMTkyMzI2NTVaMC8xLTArBgNVBAMT
+JG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2YzBZMBMGByqGSM49
+AgEGCCqGSM49AwEHA0IABIk1YZK4gZLLlluVtzL/S/dfJtQRAmh1Je2Vfz89qvOM
+GPWhG8Xtjyd3Ntg7RBc4PXpjbq7lUufxy/oWp88+KPqjgfswgfgwDgYDVR0PAQH/
+BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMAwGA1UdEwEB/wQCMAAwHwYDVR0j
+BBgwFoAUXZzsgcecviPCXlVdrBw/tUEC2uYwgaEGA1UdEQSBmTCBloIybWFyaWFk
+Yi1vcGVyYXRvci13ZWJob29rLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWyCJG1h
+cmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2Y4IgbWFyaWFkYi1vcGVy
+YXRvci13ZWJob29rLmRlZmF1bHSCGG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vazAK
+BggqhkjOPQQDAgNHADBEAiBSWY1rVufSE+3i0w553uJGJCC4Fpa6cvRPEti8X3Kp
+1AIgG0qN5IT9EsRZaY4J2vBYsbN5LL+qRI5N0XGYqVWXuD8=
+-----END CERTIFICATE-----			
+`),
+					TLSKeyKey: []byte("invalid-key"),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Valid TLS Secret",
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Type: corev1.SecretTypeTLS,
+				Data: map[string][]byte{
+					TLSCertKey: []byte(`-----BEGIN CERTIFICATE-----
+MIICXzCCAgagAwIBAgIRAIBgotjwHCDFrV2H9FWQrYIwCgYIKoZIzj0EAwIwNjEZ
+MBcGA1UEChMQbWFyaWFkYi1vcGVyYXRvcjEZMBcGA1UEAxMQbWFyaWFkYi1vcGVy
+YXRvcjAeFw0yNDEyMTkxNjI2NTVaFw0yNTEyMTkyMzI2NTVaMC8xLTArBgNVBAMT
+JG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2YzBZMBMGByqGSM49
+AgEGCCqGSM49AwEHA0IABIk1YZK4gZLLlluVtzL/S/dfJtQRAmh1Je2Vfz89qvOM
+GPWhG8Xtjyd3Ntg7RBc4PXpjbq7lUufxy/oWp88+KPqjgfswgfgwDgYDVR0PAQH/
+BAQDAgWgMBMGA1UdJQQMMAoGCCsGAQUFBwMBMAwGA1UdEwEB/wQCMAAwHwYDVR0j
+BBgwFoAUXZzsgcecviPCXlVdrBw/tUEC2uYwgaEGA1UdEQSBmTCBloIybWFyaWFk
+Yi1vcGVyYXRvci13ZWJob29rLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWyCJG1h
+cmlhZGItb3BlcmF0b3Itd2ViaG9vay5kZWZhdWx0LnN2Y4IgbWFyaWFkYi1vcGVy
+YXRvci13ZWJob29rLmRlZmF1bHSCGG1hcmlhZGItb3BlcmF0b3Itd2ViaG9vazAK
+BggqhkjOPQQDAgNHADBEAiBSWY1rVufSE+3i0w553uJGJCC4Fpa6cvRPEti8X3Kp
+1AIgG0qN5IT9EsRZaY4J2vBYsbN5LL+qRI5N0XGYqVWXuD8=
+-----END CERTIFICATE-----			
+`),
+					TLSKeyKey: []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIAdp3iKnNA1kO2Ep5Hw7owMcm06SecFGdOqW/vO4k2AjoAoGCCqGSM49
+AwEHoUQDQgAEiTVhkriBksuWW5W3Mv9L918m1BECaHUl7ZV/Pz2q84wY9aEbxe2P
+J3c22DtEFzg9emNuruVS5/HL+hanzz4o+g==
+-----END EC PRIVATE KEY-----
+`),
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewKeyPairFromTLSSecret(tt.secret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewKeyPairFromTLSSecret() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNewKeyPairFromTemplate(t *testing.T) {
+	// Subject: CN=mariadb-operator
+	caCertPEm := []byte(`-----BEGIN CERTIFICATE-----
+MIIByjCCAW+gAwIBAgIQInuHavw36CvWL7osylp/9jAKBggqhkjOPQQDAjA2MRkw
+FwYDVQQKExBtYXJpYWRiLW9wZXJhdG9yMRkwFwYDVQQDExBtYXJpYWRiLW9wZXJh
+dG9yMB4XDTI0MTIxOTE2MjY1NVoXDTI4MTIxOTE3MjY1NVowNjEZMBcGA1UEChMQ
+bWFyaWFkYi1vcGVyYXRvcjEZMBcGA1UEAxMQbWFyaWFkYi1vcGVyYXRvcjBZMBMG
+ByqGSM49AgEGCCqGSM49AwEHA0IABIx2WSuRfc98PRJB8+7IkFjLBh0jqdQXWwLt
+HW2tYw+MrFJthf93kcDH122iAUsjZ0/nvf4JR0LFJmFS7uTLJJijXzBdMA4GA1Ud
+DwEB/wQEAwICpDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBRdnOyBx5y+I8Je
+VV2sHD+1QQLa5jAbBgNVHREEFDASghBtYXJpYWRiLW9wZXJhdG9yMAoGCCqGSM49
+BAMCA0kAMEYCIQClC63wr9jTwqhd8DKuKN5riMgoW4vXxvbsBfKuoSPvdAIhALSx
+2Ky+mUAol0I2FdkaeUr3r5AObHsr5cH4OdFszF6K
+-----END CERTIFICATE-----
+`)
+	caPrivateKeyPEM := []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIOfniC62mKKWIuszxytrnSOZhSZNYhdisDwFcOFOXrAxoAoGCCqGSM49
+AwEHoUQDQgAEjHZZK5F9z3w9EkHz7siQWMsGHSOp1BdbAu0dba1jD4ysUm2F/3eR
+wMfXbaIBSyNnT+e9/glHQsUmYVLu5MskmA==
+-----END EC PRIVATE KEY-----
+`)
+	caKeyPair, err := NewKeyPair(caCertPEm, caPrivateKeyPEM)
+	if err != nil {
+		t.Fatalf("unexpected error generating CA keypair: %v", err)
+	}
+
+	tests := []struct {
+		name           string
+		tpl            *x509.Certificate
+		caKeyPair      *KeyPair
+		opts           []KeyPairOpt
+		wantErr        bool
+		wantCommonName string
+		wantIssuer     string
+	}{
+		{
+			name: "Invalid CA",
+			tpl: &x509.Certificate{
+				SerialNumber: big.NewInt(1),
+				Subject: pkix.Name{
+					CommonName: "cert",
+				},
+				NotBefore:             time.Now(),
+				NotAfter:              time.Now().Add(defaultCertValidityDuration),
+				KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+				BasicConstraintsValid: true,
+			},
+			caKeyPair: &KeyPair{
+				CertPEM: []byte("invalid-cert"),
+				KeyPEM:  []byte("invalid-key"),
+			},
+			wantErr:        true,
+			wantCommonName: "",
+			wantIssuer:     "",
+		},
+		{
+			name: "Self-signed CA",
+			tpl: &x509.Certificate{
+				SerialNumber: big.NewInt(1),
+				Subject: pkix.Name{
+					CommonName: "ca",
+				},
+				NotBefore:             time.Now(),
+				NotAfter:              time.Now().Add(-defaultCertValidityDuration), // Invalid NotAfter
+				KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment | x509.KeyUsageCertSign,
+				BasicConstraintsValid: true,
+				IsCA:                  true,
+			},
+			caKeyPair:      nil,
+			wantErr:        false,
+			wantCommonName: "ca",
+			wantIssuer:     "ca",
+		},
+		{
+			name: "Leaf certificate",
+			tpl: &x509.Certificate{
+				SerialNumber: big.NewInt(1),
+				Subject: pkix.Name{
+					CommonName: "cert",
+				},
+				NotBefore:             time.Now(),
+				NotAfter:              time.Now().Add(defaultCertValidityDuration),
+				KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+				BasicConstraintsValid: true,
+			},
+			caKeyPair:      caKeyPair,
+			wantErr:        false,
+			wantCommonName: "cert",
+			wantIssuer:     "mariadb-operator",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			keyPair, err := NewKeyPairFromTemplate(tt.tpl, tt.caKeyPair)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewKeyPairFromTemplate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantErr {
+				return
+			}
+
+			certs, err := keyPair.Certificates()
+			if err != nil {
+				t.Errorf("error getting certificates: %v", err)
+			}
+			cert := certs[0]
+
+			if cert.Subject.CommonName != tt.wantCommonName {
+				t.Errorf("CommonName = %v, want %v", cert.Subject.CommonName, tt.wantCommonName)
+			}
+			if cert.Issuer.CommonName != tt.wantIssuer {
+				t.Errorf("Issuer = %v, want %v", cert.Issuer.CommonName, tt.wantIssuer)
+			}
+		})
 	}
 }
