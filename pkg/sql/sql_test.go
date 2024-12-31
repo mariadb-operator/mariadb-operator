10c781ba442057ea01872ad8244e5a94975b35c4
mmontes11 <martin11lrx@gmail.com>
2024-12-31 11:34:57 +0100

    Added tests for require subquery

--- pkg/sql/sql_test.go
+++ pkg/sql/sql_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	mariadbv1alpha1 "github.com/mariadb-operator/mariadb-operator/api/v1alpha1"
+	"k8s.io/utils/ptr"
 )
 
 func TestBuildChangeMasterQuery(t *testing.T) {
@@ -102,6 +104,96 @@ MASTER_SSL_VERIFY_SERVER_CERT=1;
 
 			if diff := cmp.Diff(query, tt.wantQuery); diff != "" {
 				t.Errorf("unexpected query (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestRequireQuery(t *testing.T) {
+	tests := []struct {
+		name      string
+		require   *mariadbv1alpha1.TLSRequirements
+		wantQuery string
+		wantErr   bool
+	}{
+		{
+			name:      "nil",
+			require:   nil,
+			wantQuery: "",
+			wantErr:   true,
+		},
+		{
+			name:      "empty",
+			require:   &mariadbv1alpha1.TLSRequirements{},
+			wantQuery: "",
+			wantErr:   true,
+		},
+		{
+			name: "SSL",
+			require: &mariadbv1alpha1.TLSRequirements{
+				SSL: ptr.To(true),
+			},
+			wantQuery: "REQUIRE SSL",
+			wantErr:   false,
+		},
+		{
+			name: "X509",
+			require: &mariadbv1alpha1.TLSRequirements{
+				X509: ptr.To(true),
+			},
+			wantQuery: "REQUIRE X509",
+			wantErr:   false,
+		},
+		{
+			name: "Issuer",
+			require: &mariadbv1alpha1.TLSRequirements{
+				Issuer: ptr.To("/CN=mariadb-galera-ca"),
+			},
+			wantQuery: "REQUIRE ISSUER '/CN=mariadb-galera-ca'",
+			wantErr:   false,
+		},
+		{
+			name: "Subject",
+			require: &mariadbv1alpha1.TLSRequirements{
+				Subject: ptr.To("/CN=mariadb-galera-client"),
+			},
+			wantQuery: "REQUIRE SUBJECT '/CN=mariadb-galera-client'",
+			wantErr:   false,
+		},
+		{
+			name: "Issuer and Subject",
+			require: &mariadbv1alpha1.TLSRequirements{
+				Issuer:  ptr.To("/CN=mariadb-galera-ca"),
+				Subject: ptr.To("/CN=mariadb-galera-client"),
+			},
+			wantQuery: "REQUIRE ISSUER '/CN=mariadb-galera-ca' AND SUBJECT '/CN=mariadb-galera-client'",
+			wantErr:   false,
+		},
+		{
+			name: "Multiple",
+			require: &mariadbv1alpha1.TLSRequirements{
+				SSL:     ptr.To(true),
+				X509:    ptr.To(true),
+				Issuer:  ptr.To("/CN=mariadb-galera-ca"),
+				Subject: ptr.To("/CN=mariadb-galera-client"),
+			},
+			wantQuery: "",
+			wantErr:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotQuery, err := requireQuery(tt.require)
+
+			if tt.wantErr && err == nil {
+				t.Error("expect error to have occurred, got nil")
+			}
+			if !tt.wantErr && err != nil {
+				t.Errorf("expect error to not have occurred, got: %v", err)
+			}
+			if diff := cmp.Diff(tt.wantQuery, gotQuery); diff != "" {
+				t.Errorf("unexpected bundle content (-want +got):\n%s", diff)
 			}
 		})
 	}
