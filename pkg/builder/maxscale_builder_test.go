12e7b53297bb2d73d51513127751affd67786ac8
mmontes11 <martin11lrx@gmail.com>
2025-01-08 21:55:52 +0100

    Added tests for TLS builders

--- pkg/builder/maxscale_builder_test.go
+++ pkg/builder/maxscale_builder_test.go
@@ -60,3 +60,67 @@ func TestMaxScaleMeta(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxScaleTLS(t *testing.T) {
+	builder := newDefaultTestBuilder(t)
+	key := types.NamespacedName{
+		Name: "maxscale",
+	}
+	tests := []struct {
+		name    string
+		mariadb *mariadbv1alpha1.MariaDB
+		mdbmxs  *mariadbv1alpha1.MariaDBMaxScaleSpec
+		wantTLS *mariadbv1alpha1.MaxScaleTLS
+	}{
+		{
+			name: "tls enabled in MariaDB",
+			mariadb: &mariadbv1alpha1.MariaDB{
+				Spec: mariadbv1alpha1.MariaDBSpec{
+					TLS: &mariadbv1alpha1.TLS{
+						Enabled: true,
+					},
+				},
+			},
+			mdbmxs:  &mariadbv1alpha1.MariaDBMaxScaleSpec{},
+			wantTLS: &mariadbv1alpha1.MaxScaleTLS{Enabled: true},
+		},
+		{
+			name: "tls not enabled in MariaDB",
+			mariadb: &mariadbv1alpha1.MariaDB{
+				Spec: mariadbv1alpha1.MariaDBSpec{},
+			},
+			mdbmxs:  &mariadbv1alpha1.MariaDBMaxScaleSpec{},
+			wantTLS: nil,
+		},
+		{
+			name: "tls explicitly set in MaxScaleSpec",
+			mariadb: &mariadbv1alpha1.MariaDB{
+				Spec: mariadbv1alpha1.MariaDBSpec{},
+			},
+			mdbmxs: &mariadbv1alpha1.MariaDBMaxScaleSpec{
+				TLS: &mariadbv1alpha1.MaxScaleTLS{Enabled: true},
+			},
+			wantTLS: &mariadbv1alpha1.MaxScaleTLS{Enabled: true},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mxs, err := builder.BuildMaxScale(key, tt.mariadb, tt.mdbmxs)
+			if err != nil {
+				t.Fatalf("unexpected error building MaxScale: %v", err)
+			}
+			if tt.wantTLS == nil {
+				if mxs.Spec.TLS != nil {
+					t.Errorf("expected TLS to be nil, got %v", mxs.Spec.TLS)
+				}
+			} else {
+				if mxs.Spec.TLS == nil {
+					t.Errorf("expected TLS to be %v, got nil", tt.wantTLS)
+				} else if mxs.Spec.TLS.Enabled != tt.wantTLS.Enabled {
+					t.Errorf("expected TLS enabled to be %v, got %v", tt.wantTLS.Enabled, mxs.Spec.TLS.Enabled)
+				}
+			}
+		})
+	}
+}
