973bc0d6c0288827327a77c6eb285086ac29ecec
mmontes11 <martin11lrx@gmail.com>
2025-01-09 12:10:07 +0100

    Added TLS with cert-manager integration tests

--- internal/controller/mariadb_controller_replication_test.go
+++ internal/controller/mariadb_controller_replication_test.go
@@ -134,7 +134,7 @@ var _ = Describe("MariaDB replication", Ordered, func() {
 		By("Creating MariaDB with replication")
 		Expect(k8sClient.Create(testCtx, mdb)).To(Succeed())
 		DeferCleanup(func() {
-			deleteMariadb(key)
+			deleteMariadb(key, false)
 		})
 	})
 
