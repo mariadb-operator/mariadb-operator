37e44ea62f142476c477408029926fa91f7f5322
mmontes11 <martin11lrx@gmail.com>
2025-03-22 15:27:28 +0100

    Migrate MaxScale webhook to controller-runtime 0.20.3

--- internal/webhook/v1alpha1/webhook.go
+++ internal/webhook/v1alpha1/webhook.go
@@ -1,8 +1,13 @@
 package v1alpha1
 
 import (
+	"fmt"
+
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	"github.com/mariadb-operator/mariadb-operator/api/v1alpha1"
 	"github.com/mariadb-operator/mariadb-operator/pkg/webhook"
 	cron "github.com/robfig/cron/v3"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 var (
@@ -13,3 +18,35 @@ var (
 		cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow,
 	)
 )
+
+type tlsValidationItem struct {
+	tlsValue            interface{}
+	caSecretRef         *v1alpha1.LocalObjectReference
+	caFieldPath         string
+	certSecretRef       *v1alpha1.LocalObjectReference
+	certFieldPath       string
+	certIssuerRef       *cmmeta.ObjectReference
+	certIssuerFieldPath string
+}
+
+func validateTLSCert(item *tlsValidationItem) error {
+	if item.certSecretRef != nil && item.certIssuerRef != nil {
+		return field.Invalid(
+			field.NewPath("spec").Child("tls"),
+			item.tlsValue,
+			fmt.Sprintf(
+				"'%s' and '%s' are mutually exclusive. Only one of them must be set at a time.",
+				item.certFieldPath,
+				item.certIssuerFieldPath,
+			),
+		)
+	}
+	if item.caSecretRef == nil && item.certSecretRef != nil {
+		return field.Invalid(
+			field.NewPath("spec").Child("tls"),
+			item.tlsValue,
+			fmt.Sprintf("'%s' must be set when '%s' is set", item.caFieldPath, item.certFieldPath),
+		)
+	}
+	return nil
+}
