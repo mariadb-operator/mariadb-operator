0e3c0a3e21024518186e3a0d933e3172f2211a5f
mmontes11 <martin11lrx@gmail.com>
2025-06-09 16:45:07 +0200

    Fix webhook tests

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -480,8 +480,12 @@ var rootCmd = &cobra.Command{
 				setupLog.Error(err, "Unable to create webhook", "webhook", "Backup")
 				os.Exit(1)
 			}
+			if err = webhookv1alpha1.SetupPhysicalBackupWebhookWithManager(mgr); err != nil {
+				setupLog.Error(err, "Unable to create webhook", "webhook", "PhysicalBackup")
+				os.Exit(1)
+			}
 			if err = webhookv1alpha1.SetupRestoreWebhookWithManager(mgr); err != nil {
-				setupLog.Error(err, "Unable to create webhook", "webhook", "restore")
+				setupLog.Error(err, "Unable to create webhook", "webhook", "Restore")
 				os.Exit(1)
 			}
 			if err = webhookv1alpha1.SetupUserWebhookWithManager(mgr); err != nil {
