e88d7c78c418bea3a9b9f4c220ada1b1fa88631b
mmontes11 <martin11lrx@gmail.com>
2025-06-20 15:02:58 +0200

    Added tests for PVC and VolumeSnapshot builders

--- pkg/builder/volumesnapshot_builder.go
+++ pkg/builder/volumesnapshot_builder.go
@@ -10,7 +10,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
-// TODO: test
 func (b *Builder) BuildVolumeSnapshot(key types.NamespacedName, backup *mariadbv1alpha1.PhysicalBackup,
 	pvcKey types.NamespacedName) (*volumesnapshotv1.VolumeSnapshot, error) {
 	if backup.Spec.Storage.VolumeSnapshot == nil {
