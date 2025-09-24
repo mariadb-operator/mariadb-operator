4e85067372bf5cfdeb49b1fe6b7fc4d58aed72d2
Stefan Genov <sgenov94@gmail.com>
2025-09-24 14:37:12 +0300

    refactor: Replication defaults moved to `MariaDB.SetDefaults`
    The are no longer going to be defaulted every time

--- api/v1alpha1/mariadb_replication_types.go
+++ api/v1alpha1/mariadb_replication_types.go
@@ -96,9 +96,9 @@ type PrimaryReplication struct {
 	AutomaticFailoverDelay *metav1.Duration `json:"automaticFailoverDelay,omitempty"`
 }
 
-// FillWithDefaults fills the current PrimaryReplication object with DefaultReplicationSpec.
+// SetDefaults fills the current PrimaryReplication object with DefaultReplicationSpec.
 // This enables having minimal PrimaryReplication objects and provides sensible defaults.
-func (r *PrimaryReplication) FillWithDefaults() {
+func (r *PrimaryReplication) SetDefaults() {
 	if r.PodIndex == nil {
 		index := *DefaultReplicationSpec.Primary.PodIndex
 		r.PodIndex = &index
@@ -147,9 +147,9 @@ type ReplicaReplication struct {
 	SyncTimeout *metav1.Duration `json:"syncTimeout,omitempty"`
 }
 
-// FillWithDefaults fills the current ReplicaReplication object with DefaultReplicationSpec.
+// SetDefaults fills the current ReplicaReplication object with DefaultReplicationSpec.
 // This enables having minimal ReplicaReplication objects and provides sensible defaults.
-func (r *ReplicaReplication) FillWithDefaults() {
+func (r *ReplicaReplication) SetDefaults() {
 	if r.WaitPoint == nil {
 		waitPoint := *DefaultReplicationSpec.Replica.WaitPoint
 		r.WaitPoint = &waitPoint
@@ -252,20 +252,20 @@ type ReplicationSpec struct {
 	InitContainer InitContainer `json:"initContainer,omitempty"`
 }
 
-// FillWithDefaults fills the current ReplicationSpec object with DefaultReplicationSpec.
-// This enables having minimal ReplicationSpec objects and provides sensible defaults.
-func (r *ReplicationSpec) FillWithDefaults() {
+// SetDefaults fills the current Replication object with DefaultReplicationSpec.
+// This enables having minimal Replication objects and provides sensible defaults.
+func (r *Replication) SetDefaults() error {
 	if r.Primary == nil {
 		primary := *DefaultReplicationSpec.Primary
 		r.Primary = &primary
 	} else {
-		r.Primary.FillWithDefaults()
+		r.Primary.SetDefaults()
 	}
 	if r.Replica == nil {
 		replica := *DefaultReplicationSpec.Replica
 		r.Replica = &replica
 	} else {
-		r.Replica.FillWithDefaults()
+		r.Replica.SetDefaults()
 	}
 	if r.SyncBinlog == nil {
 		syncBinlog := *DefaultReplicationSpec.SyncBinlog
@@ -275,6 +275,8 @@ func (r *ReplicationSpec) FillWithDefaults() {
 		probesEnabled := *DefaultReplicationSpec.ProbesEnabled
 		r.ProbesEnabled = &probesEnabled
 	}
+
+	return nil
 }
 
 var (
