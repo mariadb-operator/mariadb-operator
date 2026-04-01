10daa2717adac09621e95e26e4d4f5943efea8dd
mmontes11 <martin11lrx@gmail.com>
2026-04-01 14:02:46 +0200

    Support for readonly in maintenance mode
    Co-authored-by: Stefan Genov <sgenov94@gmail.com>

--- pkg/controller/maintenance/controller.go
+++ pkg/controller/maintenance/controller.go
@@ -4,10 +4,7 @@ import (
 	"context"
 
 	mariadbv1alpha1 "github.com/mariadb-operator/mariadb-operator/v26/api/v1alpha1"
-	"github.com/mariadb-operator/mariadb-operator/v26/pkg/builder"
-	"github.com/mariadb-operator/mariadb-operator/v26/pkg/controller/service"
 	"github.com/mariadb-operator/mariadb-operator/v26/pkg/refresolver"
-	"github.com/mariadb-operator/mariadb-operator/v26/pkg/sql"
 	"k8s.io/client-go/tools/events"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -16,39 +13,37 @@ import (
 
 type MaintenanceReconciler struct {
 	client.Client
-	recorder          events.EventRecorder
-	builder           *builder.Builder
-	refResolver       *refresolver.RefResolver
-	serviceReconciler *service.ServiceReconciler
+	recorder    events.EventRecorder
+	refResolver *refresolver.RefResolver
 }
 
-func NewMaintenanceReconciler(client client.Client, recorder events.EventRecorder, builder *builder.Builder) *MaintenanceReconciler {
+func NewMaintenanceReconciler(client client.Client, recorder events.EventRecorder) *MaintenanceReconciler {
 	r := &MaintenanceReconciler{
 		Client:   client,
 		recorder: recorder,
-		builder:  builder,
 	}
 	if r.refResolver == nil {
 		r.refResolver = refresolver.New(client)
-	}
-	if r.serviceReconciler == nil {
-		r.serviceReconciler = service.NewServiceReconciler(client)
 	}
 	return r
 }
 
 func (r *MaintenanceReconciler) Reconcile(ctx context.Context, mariadb *mariadbv1alpha1.MariaDB) (ctrl.Result, error) {
-	if !mariadb.IsMaintenanceModeEnabled() {
-		return ctrl.Result{}, nil // TODO: branch needed for readonly
+	// We are in an early reconciliation stage, skipping. Pods are still coming up, new events will be triggered.
+	if mariadb.Status.CurrentPrimary == nil || mariadb.Status.CurrentPrimaryPodIndex == nil {
+		return ctrl.Result{}, nil
 	}
 	logger := log.FromContext(ctx).WithName("maintenance")
 
-	clientSet := sql.NewClientSet(mariadb, r.refResolver)
-	defer clientSet.Close()
-
-	if result, err := r.reconcileDrainConnections(ctx, mariadb, clientSet, logger); !result.IsZero() || err != nil {
-		return result, err
+	if !mariadb.IsMaintenanceModeEnabled() {
+		return r.reconcileReadOnly(ctx, mariadb, logger)
 	}
 
+	if result, err := r.reconcileDrainConnections(ctx, mariadb, logger); !result.IsZero() || err != nil {
+		return result, err
+	}
+	if result, err := r.reconcileReadOnly(ctx, mariadb, logger); !result.IsZero() || err != nil {
+		return result, err
+	}
 	return ctrl.Result{}, nil
 }
