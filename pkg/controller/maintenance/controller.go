e0ceafef72725e276b5a088a8b2612d314f04dfb
mmontes11 <martin11lrx@gmail.com>
2026-04-01 16:07:32 +0200

    Added guard condition and other maintenance mode improvements. Added examples
    Co-authored-by: Stefan Genov <sgenov94@gmail.com>

--- pkg/controller/maintenance/controller.go
+++ pkg/controller/maintenance/controller.go
@@ -3,6 +3,7 @@ package maintenance
 import (
 	"context"
 
+	"github.com/go-logr/logr"
 	mariadbv1alpha1 "github.com/mariadb-operator/mariadb-operator/v26/api/v1alpha1"
 	"github.com/mariadb-operator/mariadb-operator/v26/pkg/refresolver"
 	"k8s.io/client-go/tools/events"
@@ -29,16 +30,13 @@ func NewMaintenanceReconciler(client client.Client, recorder events.EventRecorde
 }
 
 func (r *MaintenanceReconciler) Reconcile(ctx context.Context, mariadb *mariadbv1alpha1.MariaDB) (ctrl.Result, error) {
-	// We are in an early reconciliation stage, skipping. Pods are still coming up, new events will be triggered.
-	if mariadb.Status.CurrentPrimary == nil || mariadb.Status.CurrentPrimaryPodIndex == nil {
+	logger := log.FromContext(ctx).WithName("maintenance")
+	if !shouldReconcileMaintenance(mariadb, logger) {
 		return ctrl.Result{}, nil
 	}
-	logger := log.FromContext(ctx).WithName("maintenance")
-
 	if !mariadb.IsMaintenanceModeEnabled() {
 		return r.reconcileReadOnly(ctx, mariadb, logger)
 	}
-
 	if result, err := r.reconcileDrainConnections(ctx, mariadb, logger); !result.IsZero() || err != nil {
 		return result, err
 	}
@@ -46,4 +44,20 @@ func (r *MaintenanceReconciler) Reconcile(ctx context.Context, mariadb *mariadbv
 		return result, err
 	}
 	return ctrl.Result{}, nil
+}
+
+func shouldReconcileMaintenance(mariadb *mariadbv1alpha1.MariaDB, logger logr.Logger) bool {
+	if mariadb.Status.CurrentPrimary == nil || mariadb.Status.CurrentPrimaryPodIndex == nil {
+		logger.V(1).Info("Current primary not set. Skippping maintenance reconciliation...")
+		return false
+	}
+	if mariadb.IsReplicationEnabled() && !mariadb.HasConfiguredReplication() {
+		logger.V(1).Info("Replication not configured. Skippping maintenance reconciliation...")
+		return false
+	}
+	if mariadb.IsGaleraEnabled() && !mariadb.HasGaleraConfiguredCondition() {
+		logger.V(1).Info("Galera not configured. Skippping maintenance reconciliation...")
+		return false
+	}
+	return true
 }
