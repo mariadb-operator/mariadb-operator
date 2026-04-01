1eafdc4264b352540ede18faa0e29cdb483d854f
YaCodesDevelopment <yacode@yacode.dev>
2026-04-01 07:47:41 +0300

    fix(recovery): SQL connection leak

--- internal/controller/mariadb_controller_primary_failover.go
+++ internal/controller/mariadb_controller_primary_failover.go
@@ -183,68 +183,70 @@ func (r *MariaDBReconciler) selectPromotedPrimaryCandidate(ctx context.Context, 
 
 	candidates := make([]promotedPrimaryCandidate, 0, len(pods))
 	for _, pod := range pods {
-		podLogger := logger.WithValues("name", pod.Name)
+		func() {
+			podLogger := logger.WithValues("name", pod.Name)
 
-		if !mdbpod.PodReady(&pod) {
-			podLogger.Info("Pod not ready. Skipping...")
-			continue
-		}
-		podIndex, err := stspkg.PodIndex(pod.Name)
-		if err != nil {
-			podLogger.Info("Invalid Pod name. Skipping...", "err", err)
-			continue
-		}
-
-		sqlClient, err := mdbsql.NewInternalClientWithPodIndex(ctx, mariadb, r.RefResolver, *podIndex, mdbsql.WithTimeout(3*time.Second))
-		if err != nil {
-			podLogger.Info("Unable to create SQL connection. Skipping...", "err", err)
-			continue
-		}
-		defer sqlClient.Close()
-
-		readOnly, err := isReadOnly(ctx, sqlClient)
-		if err != nil {
-			podLogger.Info("Unable to determine read_only state. Skipping...", "err", err)
-			continue
-		}
-		if readOnly {
-			podLogger.Info("Pod is read_only. Skipping...")
-			continue
-		}
-
-		isReplica, err := sqlClient.IsReplicationReplica(ctx)
-		if err != nil {
-			podLogger.Info("Unable to determine replica role. Skipping...", "err", err)
-			continue
-		}
-		if isReplica {
-			hasConnectedReplicas, err := sqlClient.HasConnectedReplicas(ctx)
+			if !mdbpod.PodReady(&pod) {
+				podLogger.Info("Pod not ready. Skipping...")
+				return
+			}
+			podIndex, err := stspkg.PodIndex(pod.Name)
 			if err != nil {
-				podLogger.Info("Unable to determine connected replicas. Skipping...", "err", err)
-				continue
+				podLogger.Info("Invalid Pod name. Skipping...", "err", err)
+				return
 			}
-			if !hasConnectedReplicas {
-				podLogger.Info("Writable Pod still behaves as a replica. Skipping...")
-				continue
-			}
-		}
 
-		candidate := promotedPrimaryCandidate{name: pod.Name}
-		if gtidDomainId, err := sqlClient.GtidDomainId(ctx); err != nil {
-			podLogger.Info("Error getting GTID domain ID. Skipping GTID ordering...", "err", err)
-		} else if currentPos, err := sqlClient.GtidCurrentPos(ctx); err != nil {
-			podLogger.Info("Error getting GTID current position. Skipping GTID ordering...", "err", err)
-		} else if currentPos != "" {
-			gtidCurrentPos, err := mariadbrepl.ParseGtidWithDomainId(currentPos, *gtidDomainId, podLogger)
+			sqlClient, err := mdbsql.NewInternalClientWithPodIndex(ctx, mariadb, r.RefResolver, *podIndex, mdbsql.WithTimeout(3*time.Second))
 			if err != nil {
-				podLogger.Info("Error parsing GTID current position. Skipping GTID ordering...", "err", err)
-			} else {
-				candidate.gtidCurrentPos = gtidCurrentPos
+				podLogger.Info("Unable to create SQL connection. Skipping...", "err", err)
+				return
 			}
-		}
+			defer sqlClient.Close()
 
-		podLogger.Info("Found externally promoted primary candidate")
-		candidates = append(candidates, candidate)
+			readOnly, err := isReadOnly(ctx, sqlClient)
+			if err != nil {
+				podLogger.Info("Unable to determine read_only state. Skipping...", "err", err)
+				return
+			}
+			if readOnly {
+				podLogger.Info("Pod is read_only. Skipping...")
+				return
+			}
+
+			isReplica, err := sqlClient.IsReplicationReplica(ctx)
+			if err != nil {
+				podLogger.Info("Unable to determine replica role. Skipping...", "err", err)
+				return
+			}
+			if isReplica {
+				hasConnectedReplicas, err := sqlClient.HasConnectedReplicas(ctx)
+				if err != nil {
+					podLogger.Info("Unable to determine connected replicas. Skipping...", "err", err)
+					return
+				}
+				if !hasConnectedReplicas {
+					podLogger.Info("Writable Pod still behaves as a replica. Skipping...")
+					return
+				}
+			}
+
+			candidate := promotedPrimaryCandidate{name: pod.Name}
+			if gtidDomainId, err := sqlClient.GtidDomainId(ctx); err != nil {
+				podLogger.Info("Error getting GTID domain ID. Skipping GTID ordering...", "err", err)
+			} else if currentPos, err := sqlClient.GtidCurrentPos(ctx); err != nil {
+				podLogger.Info("Error getting GTID current position. Skipping GTID ordering...", "err", err)
+			} else if currentPos != "" {
+				gtidCurrentPos, err := mariadbrepl.ParseGtidWithDomainId(currentPos, *gtidDomainId, podLogger)
+				if err != nil {
+					podLogger.Info("Error parsing GTID current position. Skipping GTID ordering...", "err", err)
+				} else {
+					candidate.gtidCurrentPos = gtidCurrentPos
+				}
+			}
+
+			podLogger.Info("Found externally promoted primary candidate")
+			candidates = append(candidates, candidate)
+		}()
 	}
 
 	if len(candidates) == 0 {
