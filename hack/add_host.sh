1fe27aa7ec144046a3ad01e15c289eade58eb49b
mmontes11 <martin11lrx@gmail.com>
2026-04-07 19:38:09 +0200

    Added initial multi-cluster types, replication configuration, examples and local connectivity.
    Co-authored-by: Stefan Genov <sgenov94@gmail.com>

--- hack/add_host.sh
+++ hack/add_host.sh
@@ -2,9 +2,14 @@
 
 set -euo pipefail
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <3 octet> <4 octet> <hostname>"
+    exit 1
+fi
+
 CIDR_PREFIX=$(go run ./hack/get_kind_cidr_prefix/main.go)
-IP="${CIDR_PREFIX}.0.$1"
-HOSTNAME=$2
+IP="${CIDR_PREFIX}.$1.$2"
+HOSTNAME=$3
 
 if grep -q "^$IP\s*$HOSTNAME" /etc/hosts; then
   echo "\"$HOSTNAME\" host already exists in /etc/hosts"
