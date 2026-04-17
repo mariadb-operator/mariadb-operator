e3e07c62d713d4f1825b9ec30b21572b46e7076a
mmontes11 <martin11lrx@gmail.com>
2026-04-17 20:19:21 +0200

    Ignoring error 1948 when resetting gtid_slave_pos during primary configuration.
    This error could happen when log_slave_updates=0N (multi-cluster, PITR),
    when the replica to be promoted already has binary logs.
    If returned, this error will completely block switchover/failover operations.
    Error 1948 (HY000): Specified value for @@gtid_slave_pos contains no value for
    replication domain 0. This conflicts with the binary log which contains GTID
    0-11-1176. If MASTER_GTID_POS=CURRENT_POS is used, the binlog position will
    override the new value of @@gtid_slave_pos'
    
    Co-authored-by: Stefan Genov <sgenov94@gmail.com>

--- pkg/sql/error.go
+++ pkg/sql/error.go
@@ -6,11 +6,17 @@ import (
 )
 
 var (
-	// Ref: https://mariadb.com/docs/server/reference/error-codes/mariadb-error-codes-1000-to-1099/e1095
-	SQLYouAreNotOwnerOfThread = 1095
 	// ERROR 1617 (HY000): There is no master connection  '<conn_name'
 	// Ref: https://mariadb.com/docs/server/reference/error-codes/mariadb-error-codes-1600-to-1699/e1617
 	SQLConnectionNotExists = 1617
+	// Error 1948 (HY000): Specified value for @@gtid_slave_pos contains no value for
+	// replication domain 0. This conflicts with the binary log which contains GTID
+	// 0-11-1176. If MASTER_GTID_POS=CURRENT_POS is used, the binlog position will
+	// override the new value of @@gtid_slave_pos'
+	// Ref: https://mariadb.com/docs/server/reference/error-codes/mariadb-error-codes-1900-to-1999/e1948
+	SQLGtidSlavePosNoValueForDomain = 1948
+	// Ref: https://mariadb.com/docs/server/reference/error-codes/mariadb-error-codes-1000-to-1099/e1095
+	SQLYouAreNotOwnerOfThread = 1095
 )
 
 // IsSQLErrorNumber checks if the error's string message contains the pattern
@@ -33,6 +39,11 @@ func returnNilIfErrorIsNumber(err error, number int) error {
 // Connection Not Exists
 func IsConnectionNotExists(err error) bool {
 	return IsSQLErrorNumber(err, SQLConnectionNotExists)
+}
+
+// Cannot set `gtid_slave_pos`
+func IsGtidSlavePosNoValueForDomain(err error) bool {
+	return IsSQLErrorNumber(err, SQLGtidSlavePosNoValueForDomain)
 }
 
 // You are not owner of thread
