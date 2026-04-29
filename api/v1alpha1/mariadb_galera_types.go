614a4bcc3c6b83a33cad46d6b46f8bbbbe3f287c
mmontes11 <martin11lrx@gmail.com>
2026-04-29 19:45:46 +0200

    Added multi-cluster types and config file
    Co-authored-by: Stefan Genov <sgenov94@gmail.com>

--- api/v1alpha1/mariadb_galera_types.go
+++ api/v1alpha1/mariadb_galera_types.go
@@ -373,6 +373,25 @@ type GaleraSpec struct {
 	// +optional
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:advanced"}
 	Config GaleraConfig `json:"config,omitempty"`
+	// GtidDomainID is the domain ID to be used in GTID mode, enabled when the multi-cluster topology is used.
+	// For example: if you set this to `0`, the 'wsrep_gtid_domain_id' will be 0, while the replicas (if 3) will have 'gtid_domain_id' 1,2,3.
+	// Make sure it has a different value on each the member of a multi-cluster topology.
+	// See: https://mariadb.com/docs/galera-cluster/high-availability/using-mariadb-replication-with-mariadb-galera-cluster/configuring-mariadb-replication-between-two-mariadb-galera-clusters
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:advanced"}
+	GtidDomainID *int `json:"gtidDomainId,omitempty" webhook:"inmutable"`
+	// ServerID is the server ID to be used in GTID mode, enabled when the multi-cluster topology is used.
+	// Make sure it has a different value on each the member of a multi-cluster topology.
+	// See: https://mariadb.com/docs/galera-cluster/high-availability/using-mariadb-replication-with-mariadb-galera-cluster/configuring-mariadb-replication-between-two-mariadb-galera-clusters
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:advanced"}
+	ServerID *int `json:"serverId,omitempty" webhook:"inmutable"`
+	// ReplPasswordSecretKeyRef provides a reference to the Secret to use as password for the replication user.
+	// This will be utilized as password of the replication user, when the multi-cluster topology is enabled.
+	// By default, a random password will be generated.
+	// +optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	ReplPasswordSecretKeyRef *GeneratedSecretKeyRef `json:"replPasswordSecretKeyRef,omitempty"`
 }
 
 // GaleraBootstrapStatus indicates when and in which Pod the cluster bootstrap process has been performed.
