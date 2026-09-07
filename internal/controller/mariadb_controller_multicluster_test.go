a80582e763db03cdeef637aca339dc9597855728
Martin Montes <martin11lrx@gmail.com>
2026-09-07 20:49:52 +0000

    Revert "Remove image override from Galera multi-cluster MaxScale tests"
    This reverts commit eee18c7a0f72d2633bd19bc9dcb3b39ee191907e.

--- internal/controller/mariadb_controller_multicluster_test.go
+++ internal/controller/mariadb_controller_multicluster_test.go
@@ -363,6 +363,7 @@ var _ = Describe("MariaDB multi-cluster with Galera and MaxScale", Ordered, Labe
 				primaryMariaDBAddr,
 				galeraReplicas,
 				multiCluster,
+				withMariaDBImage("mariadb:11.8.8"),
 			),
 			multiClusterGaleraDecorator(
 				primaryGaleraGtidDomainId,
@@ -375,6 +376,7 @@ var _ = Describe("MariaDB multi-cluster with Galera and MaxScale", Ordered, Labe
 				replicaMariaDBAddr,
 				galeraReplicas,
 				multiCluster,
+				withMariaDBImage("mariadb:11.8.8"),
 			),
 			multiClusterReplicationDecorator(
 				replicaGaleraGtidDomainId,
